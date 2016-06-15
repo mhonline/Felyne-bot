fe15edce993f179d16f0868dd29b35a64a96ac0c
Reaver01 <reaver_01@gmx.com>
2016-06-16 00:28:30 +0200

    load all commands

--- felynebot.rb
+++ felynebot.rb
@@ -10,7 +10,7 @@ module FelyneBot
 	include Sys
 	require 'time'
 
-	require_relative 'commands/ping'
+Dir["#{File.dirname(__FILE__)}commands/*.rb"].each { |file| require_relative file }
 
 	clock=Time.new
 
