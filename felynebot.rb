fe66ba1d157bf1ae7a7dd703fbf1b51b8ba14f2b
Reaver01 <reaver_01@gmx.com>
2016-06-16 01:09:01 +0200

    removing storedata require

--- felynebot.rb
+++ felynebot.rb
@@ -9,9 +9,7 @@ module FelyneBot
 	require 'yaml'
 	include Sys
 	require 'time'
-
-	require_relative 'other/store_data'
-	include StoreData
+	
 	require_relative 'commands/ping'
 
 	clock=Time.new
@@ -85,6 +83,52 @@ module FelyneBot
 	    @usr_timezone
 	  end
 	end
+
+		#Method for saving arrays
+		def saveArr(ar,loc)
+			if File.exist?(loc)
+				f = File.open(loc,"w")
+			else
+				f = File.new(loc,"w")
+			end
+			f.write(ar.to_json)
+			f.close
+			puts "Saved File: [#{ar}] into [#{loc}]"
+
+		end
+
+		#Method for loading arrays
+		def loadArr(ar,loc)
+			if File.exist?(loc)
+				#puts 'Opened file'
+				f = File.open(loc,"r")
+			else
+				#puts 'No file!'
+			end
+			buff = f.read
+			ar=JSON.parse(buff)
+			#puts 'Loaded array!'
+			return ar
+		end
+
+		#Load object arrays
+		def loadObj(ar,loc)
+			if File.exist?(loc)
+		    f = File.open(loc,"r")
+		    users=YAML.load(ar)
+		    puts 'Loaded user database'
+		    f.close
+		  else
+		    puts 'No file to load!'
+		  end
+		end
+
+		#save object arrays
+		def saveObj(ar,loc)
+		  ar.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+			if File.exist?(loc) then File.open(loc, 'w') {|f| f.write(YAML.dump(ar)) }
+			else File.new(loc, 'w') {|f| f.write(YAML.dump(ar)) } end
+		end
 
 	#Creating the actual user array/loads the file there
 	users=[]
