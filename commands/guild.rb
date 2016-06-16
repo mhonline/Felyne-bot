56188d7013482d88a541f8fa9491fe856a17101b
Reaver01 <reaver_01@gmx.com>
2016-06-16 15:43:16 +0200

    max args

--- commands/guild.rb
+++ commands/guild.rb
@@ -2,7 +2,7 @@ module FelyneBot
   module Commands
     module Guild
       extend Discordrb::Commands::CommandContainer
-      command(:guild, min_args: 0, max_args: 1, description: 'Add user to guild', usage: "-guild <Guildname>", permission_level: 800) do |event, search|
+      command(:guild, min_args: 1, max_args: 1, description: 'Add user to guild', usage: "-guild <Guildname>", permission_level: 800) do |event, search|
       member = event.user.on(event.server)
 	    if search == 'Hipcheckers'
 	    role = event.server.roles.find { |role| role.name == "Hipcheckers" }
