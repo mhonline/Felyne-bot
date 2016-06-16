7322dc8297ec3b420df018e1c3b5cacb19ccfa8d
Reaver01 <reaver_01@gmx.com>
2016-06-16 15:52:06 +0200

    fixed

--- commands/guild.rb
+++ commands/guild.rb
@@ -8,55 +8,55 @@ module FelyneBot
 	    role = event.server.roles.find { |role| role.name == "Hipcheckers" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'Hyperlynx'
 	    role = event.server.roles.find { |role| role.name == "Hyperlynx" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'Stygian'
 	    role = event.server.roles.find { |role| role.name == "Stygian" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'MHOInter'
 	    role = event.server.roles.find { |role| role.name == "MHOInter" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'Discord'
 	    role = event.server.roles.find { |role| role.name == "Discord" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'SG★Hunters'
 	    role = event.server.roles.find { |role| role.name == "SG★Hunters" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == '血盟狩猎团'
 	    role = event.server.roles.find { |role| role.name == "血盟狩猎团" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == '月夜黑貓團'
 	    role = event.server.roles.find { |role| role.name == "月夜黑貓團" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == '为卿负天下、'
 	    role = event.server.roles.find { |role| role.name == "为卿负天下、" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'Abysswalker'
 	    role = event.server.roles.find { |role| role.name == "Abysswalker" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'thelegionnaires'
 	    role = event.server.roles.find { |role| role.name == "thelegionnaires" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'GatotKaca'
 	    role = event.server.roles.find { |role| role.name == "GatotKaca" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'LaTaverne'
 	    role = event.server.roles.find { |role| role.name == "LaTaverne" }
 		member.add_role(role)
   	    end
-	    if search == 'Hipcheckers'
+	    if search == 'MooGoo'
 	    role = event.server.roles.find { |role| role.name == "MooGoo" }
 		member.add_role(role)
   	    end
