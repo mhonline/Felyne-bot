c503f57fbcab2ae12c4d818e50083c20a4fbc690
Reaver01 <reaver_01@gmx.com>
2016-06-16 15:43:02 +0200

    null search isn't a thing apparently

--- commands/guild.rb
+++ commands/guild.rb
@@ -4,9 +4,6 @@ module FelyneBot
       extend Discordrb::Commands::CommandContainer
       command(:guild, min_args: 0, max_args: 1, description: 'Add user to guild', usage: "-guild <Guildname>", permission_level: 800) do |event, search|
       member = event.user.on(event.server)
-	    if search == null
-	    $bot.send_message(event.message.channel, "Guilds on server: Hipcheckers, Hyperlynx, Stygian, MHOInter, Discord, SG★Hunters, 血盟狩猎团, 月夜黑貓團, 为卿负天下、, Abysswalker, thelegionnaires, GatotKaca, LaTaverne, MooGoo")
-	    end
 	    if search == 'Hipcheckers'
 	    role = event.server.roles.find { |role| role.name == "Hipcheckers" }
 		member.add_role(role)
