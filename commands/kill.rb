901502af71d65bf5fb4c918ae98a9063e1affc93
Reaver01 <reaver_01@gmx.com>
2016-06-16 13:06:30 +0200

    fix

--- commands/kill.rb
+++ commands/kill.rb
@@ -4,8 +4,8 @@ module FelyneBot
       extend Discordrb::Commands::CommandContainer
       command(:kill, description: "Kills felyne", permission_level: 800) do |event|
         puts "Daisy... daisy, give me your answer do..."
-        bot.send_message(event.message.channel, "Daisy... daisy, give me your answer do...")
-        #bot.stop
+        $bot.send_message(event.message.channel, "Daisy... daisy, give me your answer do...")
+        $bot.stop
         exit
       end
     end
