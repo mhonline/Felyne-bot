e7d5f956129e48aff2fc34eee7184ecfe75623bd
Haplo064 <haplo064@gmail.com>
2016-06-16 18:47:53 +0930

    Add files via upload

--- felynebot.rb
+++ felynebot.rb
@@ -1,18 +1,13 @@
 module FelyneBot
-	#Simple prompt for input
-
-
-		#Open file for token/Get Token
 	require_relative 'class/info'
 	puts "Loaded info"
 
-	users=[]
-
+	$users=[]
 	info=Info.new('bot/token','bot/id')
 	token=info.token
 	id=info.id
 
-	bot = Discordrb::Commands::CommandBot.new token: token, application_id: id, prefix: '-', advanced_functionality: false
+	$bot = Discordrb::Commands::CommandBot.new token: token, application_id: id, prefix: '-', advanced_functionality: false
 	puts "Bot begun"
 
 	#Loads permissions from array
@@ -23,24 +18,22 @@ module FelyneBot
 	#Loading permissions array
 	pos=0
 	begin
-		bot.set_user_permission(permarray[pos],permarray[pos+1])
+		$bot.set_user_permission(permarray[pos],permarray[pos+1])
 		puts "Added #{permarray[pos+2]} as level #{permarray[pos+1]} user"
 		pos+=3
 	end while pos < permarray.length
 	puts "------------->Permission Loaded!"
 
-	bot.include! Commands::Ping
-	bot.include! Commands::Game
-	bot.include! Commands::Kill
-	bot.include! Commands::AddUser
-
+	$bot.include! Commands::Ping
+	$bot.include! Commands::Kill
+	$bot.include! Commands::AddUser
+	$bot.include! Commands::Game
 
 	puts "Commands Loaded"
-	bot.debug = false
-	bot.run
-	bot.run :async
-	bot.game = '-help'
+	$bot.debug = false
+	$bot.run :async
+	$bot.game = '-help'
 	puts 'Sync Confirmed.'
 	puts 'SKYNET ONLINE'
-	bot.sync
+	$bot.sync
 end
