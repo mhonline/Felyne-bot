e075ad4e36d51fef05294b960a661ac299e68b80
Reaver01 <reaver_01@gmx.com>
2016-06-16 15:17:08 +0200

    test

--- commands/guild.rb
+++ commands/guild.rb
@@ -4,8 +4,8 @@ module FelyneBot
       extend Discordrb::Commands::CommandContainer
       command(:guild, min_args: 0, max_args: 1, description: 'Add user to guild', usage: "-guild <Guildname>", permission_level: 800) do |event|
       member = event.user.on(event.server)
-      puts member.inspect
-      member.add_role(0x00000806e75d48)
+      puts event.server.roles
+      #member.add_role(0x00000806e75d48)
       end
     end
   end
