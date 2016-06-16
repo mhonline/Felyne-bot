40fa25371d327787d3dadce0a4b76d8bec67eb66
Reaver01 <reaver_01@gmx.com>
2016-06-16 13:25:48 +0200

    line swap

--- commands/color.rb
+++ commands/color.rb
@@ -3,8 +3,8 @@ module FelyneBot
     module Color
       extend Discordrb::Commands::CommandContainer
       command(:color, description: "Changes avatar randomly", permission_level: 1) do |event|
-        $bot.send_message(event.message.channel, "Changing Avatar!")
         $bot.profile.avatar = File.open("/Felyne-bot/pic/#{1 + rand(35)}.jpg")
+        $bot.send_message(event.message.channel, "Changing Avatar!")
         puts "Changing Avatar"
       end
     end
