abd87e65bb98b1cf9429a2b3677fef71be5c036b
Reaver01 <reaver_01@gmx.com>
2016-06-16 15:13:02 +0200

    test

--- commands/guild.rb
+++ commands/guild.rb
@@ -5,6 +5,7 @@ module FelyneBot
       command(:guild, min_args: 0, max_args: 1, description: 'Add user to guild', usage: "-guild <Guildname>", permission_level: 800) do |event|
       member = event.user.on(event.server)
       puts member.inspect
+      member.add_role(0x00000806e75d48)
       end
     end
   end
