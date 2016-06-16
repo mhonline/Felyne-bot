2ef811e63cbc8fc2ffde6fbb615986a1c0a01db2
Reaver01 <reaver_01@gmx.com>
2016-06-16 20:51:09 +0200

    time is reset wat

--- commands/time.rb
+++ commands/time.rb
@@ -2,15 +2,13 @@ module FelyneBot
   module Commands
     module Time
       extend Discordrb::Commands::CommandContainer
-		command(:time) do |event|
-		  t2 = Time.now.to_i
-		  t1 = Time.parse("20:00").to_i
-		  if t1 > t2 then event << "#{Time.at(t1 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next ticket reset"
-		  else event << "#{Time.at(t1 + 86400 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next ticket reset" end
-		  puts "#{clock.inspect}: #{event.user.name}: -time"
-		  end
-		end
-	  end
+      command(:time) do |event|
+        t2 = Time.now.to_i
+        t1 = Time.parse("20:00").to_i
+        if t1 > t2 then event << "#{Time.at(t1 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next ticket reset"
+        else event << "#{Time.at(t1 + 86400 - t2).strftime('**%H** hours **%M** minutes **%S** seconds')} left until the next ticket reset" end
+        puts "#{event.timestamp}: #{event.user.name}: -time"
+      end
     end
   end
 end
