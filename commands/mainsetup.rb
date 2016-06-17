dd88bb5196acf2ee9702f50c35ec09050a0f1ae0
Reaver01 <reaver_01@gmx.com>
2016-06-17 23:57:32 +0200

    time

--- commands/mainsetup.rb
+++ commands/mainsetup.rb
@@ -5,13 +5,12 @@ module FelyneBot
 			command(:mainsetup, permission_level: 1) do |event, hours, minutes|
 				h = hours.to_i
 				m = minutes.to_i
-				t1 = Time.now
-				t1 = t1.to_i
 				time = h * 3600 + m * 60
 				nowtime = Time.now
-				nowtime = t1.to_i
+				nowtime = nowtime.to_i
 				nowtime += time
 				timecode = Time.at(timecode)
+				timecode = Time.parse(timecode)
 				event << "#{Time.at(t3).strftime('%H hours %M minutes %S seconds')} left until the next exp/gift reset"
 				nil
 			end
