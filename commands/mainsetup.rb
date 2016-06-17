bb1dfdbbad07160d60a4957f21d255cd4134a988
Reaver01 <reaver_01@gmx.com>
2016-06-17 23:47:57 +0200

    capital Time

--- commands/mainsetup.rb
+++ commands/mainsetup.rb
@@ -5,7 +5,7 @@ module FelyneBot
 			command(:mainsetup, permission_level: 1) do |event, hours, minutes|
 				h = hours.to_i
 				m = minutes.to_i
-				time = h * 3600 + m * 60 + time.now
+				time = h * 3600 + m * 60 + Time.now
 				$timecode = Time.parse('time')
 				event.respond
 				nil
