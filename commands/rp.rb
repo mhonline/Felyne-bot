b19466f3141250f5a3dc24f6ed515bd6d35ddd28
Reaver01 <reaver_01@gmx.com>
2016-06-17 23:20:14 +0200

    test

--- commands/rp.rb
+++ commands/rp.rb
@@ -2,8 +2,8 @@ module FelyneBot
 	module Commands
 		module RolePlay
 			extend Discordrb::Commands::CommandContainer
-			command(:rp) do |event, phrase|
-				phrase = phrase.join(' ')
+			command(:rp) do |_event, *phrase|
+				phrase = "#{phrase.join(' ')}"
 				event << "sent **#{phrase}** to mhodiscussion"
 				bot.send_message(122526505606709257, phrase)
 				puts 'CMD: roleplay'
