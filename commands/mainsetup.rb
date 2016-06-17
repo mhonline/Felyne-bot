2a7fd71a8dd9d2a7f7a76b23e87921962b414f33
Reaver01 <reaver_01@gmx.com>
2016-06-18 00:05:12 +0200

    test

--- commands/mainsetup.rb
+++ commands/mainsetup.rb
@@ -3,15 +3,15 @@ module FelyneBot
 		module MainSetup
 			extend Discordrb::Commands::CommandContainer
 			command(:mainsetup, permission_level: 1) do |event, hours, minutes|
-				h = hours.to_i
-				m = minutes.to_i
-				timedifference = h * 3600 + m * 60
-				nowtime = Time.now.to_i
-				nowtime += time
-				timecode = Time.parse(nowtime)
-				event << "#{Time.at(timecode).strftime('%H hours %M minutes %S seconds')} left until the next exp/gift reset"
-				nil
-			end
+h = hours.to_i
+m = minutes.to_i
+timedifference = h * 3600 + m * 60
+t = Time.now
+nowtime = t.to_i
+nowtime += time
+timecode = Time.parse(nowtime)
+event << "#{Time.at(timecode).strftime('%H hours %M minutes %S seconds')} left until the next exp/gift reset"
+end
 		end
 	end
 end
