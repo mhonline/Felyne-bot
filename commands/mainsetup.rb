f6be5755483cccb2d504ad9472e428479e5697c4
Reaver01 <reaver_01@gmx.com>
2016-06-18 00:28:55 +0200

    almost final

--- commands/mainsetup.rb
+++ commands/mainsetup.rb
@@ -3,17 +3,16 @@ module FelyneBot
 		module MainSetup
 			extend Discordrb::Commands::CommandContainer
 			command(:mainsetup, permission_level: 1) do |event, hours, minutes|
-h = hours.to_i
-m = minutes.to_i
-now = Time.now
-now = now.to_i
-timediff = h * 3600 + m * 60 
-timediff += now
-timediff = Time.at (timediff)
-event << "#{h} hours #{m} minutes left until the end of maintenance"
-event << timediff
-event << now
-end
+				h = hours.to_i
+				m = minutes.to_i
+				now = Time.now
+				now = now.to_i
+				targettime = h * 3600 + m * 60 
+				targettime += now
+				targettime = Time.at (targettime)
+				$targettime = targettime
+				event << "#{h} hours #{m} minutes left until the end of maintenance"
+			end
 		end
 	end
 end
