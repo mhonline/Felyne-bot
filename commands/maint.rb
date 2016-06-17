8132292d54240a7be4490a56340d798b4fb0446b
Reaver01 <reaver_01@gmx.com>
2016-06-18 00:33:08 +0200

    drunk and tired

--- commands/maint.rb
+++ commands/maint.rb
@@ -3,7 +3,7 @@ module FelyneBot
 		module Maint
 			extend Discordrb::Commands::CommandContainer
 			command(:maint, permission_level: 1) do |event, hours, minutes|
-targettime = Time.at (targettime.to_i)
+timediff = $targettime.to_i - now.to_i
 event << "#{Time.at(timediff).strftime('%H hours %M minutes %S seconds')} left until the end of maint"
 			end
 		end
