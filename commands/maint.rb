d320b2d108fb286ff43431a18a35ab8121719d45
Reaver01 <reaver_01@gmx.com>
2016-06-18 00:48:52 +0200

    shortening

--- commands/maint.rb
+++ commands/maint.rb
@@ -3,10 +3,8 @@ module FelyneBot
 		module Maint
 			extend Discordrb::Commands::CommandContainer
 			command(:maint) do |event|
-now = Time.now
-now = now.to_i
-targettime = $targettime
-timediff = targettime - now
+now = Time.now.to_i
+timediff = $targettime - now
 event << "#{Time.at(timediff).strftime('%H hours %M minutes %S seconds')} left until the end of maint"
 			end
 		end
