2dff32ebb0f148f748ddf16c67695951dff6c9f1
Reaver01 <reaver_01@gmx.com>
2016-06-17 23:43:10 +0200

    moved nil

--- commands/userguild.rb
+++ commands/userguild.rb
@@ -5,11 +5,11 @@ module FelyneBot
 			command(:userguild, max_args: 1, min_args: 0, description: "Adds guild for user to the database.", usage: "-userguild <guildname>") do |event, guild=nil|
 				temp = $users.find_index {|s| s.id == event.user.id}
 				if  temp!=nil then $users[temp].addGuild(guild) end
-					event << "Changed guild to: #{guild}"
-					puts "#{event.timestamp}: #{event.user.name}: [userGuild] <#{guild}>"
-					saveObj($users,"userbase/users")
-				end
+				event << "Changed guild to: #{guild}"
+				puts "#{event.timestamp}: #{event.user.name}: [userGuild] <#{guild}>"
+				saveObj($users,"userbase/users")
 				nil
+			end
 		end
 	end
 end
