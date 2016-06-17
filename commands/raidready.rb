3cefb1cd18c4c9e8bfd2b64131247802c7837c49
reaver01 <reaver_01@gmx.com>
2016-06-17 21:40:21 +0200

    fixed name

--- commands/raidready.rb
+++ commands/raidready.rb
@@ -5,12 +5,14 @@ module FelyneBot
 			command(:raidready, min_args: 1, max_args: 1, description: 'Add user to Raid Ready server role', usage: "-raidready <yes/no>") do |event, search|
 				member = event.user.on(event.server)
 				if search == 'yes'
-					role = event.server.roles.find { |role| role.name == "Raid Ready" }
+					role = event.server.roles.find { |role| role.name == "RaidReady" }
 					member.add_role(role)
+					event << "Added to RaidReady Role"
 				end
 				if search == 'no'
-					role = event.server.roles.find { |role| role.name == "Raid Ready" }
+					role = event.server.roles.find { |role| role.name == "RaidReady" }
 					member.remove_role(role)
+					event << "Removed from RaidReady Role"
 				end
 			end
 		end
