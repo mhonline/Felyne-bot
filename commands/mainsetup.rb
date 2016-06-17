d175404acbe065e84b19238c5289abfab91945ad
Reaver01 <reaver_01@gmx.com>
2016-06-18 00:18:49 +0200

    this or wait

--- commands/mainsetup.rb
+++ commands/mainsetup.rb
@@ -7,10 +7,10 @@ h = hours.to_i
 m = minutes.to_i
 now = Time.now
 now = now.to_i
-timediff = h * 3600 + m * 60
+timediff = h + m 
 timediff = Time.at (timediff)
 event << "#{h} hours #{m} minutes left until the end of maintenance"
-end
+timediff
 		end
 	end
 end
