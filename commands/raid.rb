841a2ce981292a1e7b2ebdb427e8e1cae320f8db
Reaver01 <reaver_01@gmx.com>
2016-06-18 02:29:20 +0200

    File.zero

--- commands/raid.rb
+++ commands/raid.rb
@@ -5,11 +5,10 @@ module FelyneBot
 			command(:raid) do |event|
 				now = Time.now
 				now = now.to_i
-				name1=IO.readlines("bot/raid1")[0]
-				event << name1
-				if name1 == ""
+				if File.zero?("bot/raid1")
 					raid1 = "```Raid 1:  No raid set up."
 				else
+					name1=IO.readlines("bot/raid1")[0]
 					name1=name1.gsub("\n","")
 					time1=IO.readlines("bot/raid1")[1].to_i
 					timediff1 = time1 - now - 3600
