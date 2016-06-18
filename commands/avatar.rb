00287417f1fb59d3655874a7203cd95ff4e72917
Reaver01 <reaver_01@gmx.com>
2016-06-18 15:10:20 +0200

    added color picker

--- commands/avatar.rb
+++ commands/avatar.rb
@@ -2,8 +2,17 @@ module FelyneBot
 	module Commands
 		module Avatar
 			extend Discordrb::Commands::CommandContainer
-			command(:avatar, description: "Changes avatar randomly", permission_level: 1) do |event|
-				picture = 1 + rand(35)
+			command(:avatar, description: "Changes avatar randomly", permission_level: 1) do |event, text|
+				if text == ''
+					picture = 1 + rand(35)
+				else
+					if -1 > text < 36
+						picture = text
+					else
+						$bot.send_message(event.message.channel, "Invalid Option, randomizing!")
+						picture = 1 + rand(35)
+					end
+				end
 				$bot.profile.avatar = File.open("/Felyne-bot/pic/#{picture}.jpg")
 				role = event.server.roles.find { |role| role.name == "Felyne" }
 				newcolor = '0x'
