4e51d6f54e857a4c15c5c6042e5e20bf98d9396e
Reaver01 <reaver_01@gmx.com>
2016-06-18 21:28:26 +0200

    forgot to take \n from string

--- commands/raid.rb
+++ commands/raid.rb
@@ -4,23 +4,24 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(:raid, description: 'Displays raid schedule.') do |event, list|
 				t1 = Time.now
-				name1=IO.readlines("bot/raid1")[0]
-				name2=IO.readlines("bot/raid2")[0]
-				name3=IO.readlines("bot/raid3")[0]
-				name4=IO.readlines("bot/raid4")[0]
-				name5=IO.readlines("bot/raid5")[0]
+				name1 = IO.readlines("bot/raid1")[0]
+				name2 = IO.readlines("bot/raid2")[0]
+				name3 = IO.readlines("bot/raid3")[0]
+				name4 = IO.readlines("bot/raid4")[0]
+				name5 = IO.readlines("bot/raid5")[0]
 				if name1.to_s == ''
 					raid1 = "```Raid 1:  Nothing set up"
 				else
-					name1=name1.gsub("\n","")
-					r1t4=IO.readlines("bot/raid1")[1]
+					name1 = name1.gsub("\n","")
+					r1t4 = IO.readlines("bot/raid1")[1]
+					r1t4 = r1t4.gsub("\n","")
 					r1d1 = TimeDifference.between(t1, r1t4).in_days
 					r1h1 = TimeDifference.between(t1, r1t4).in_hours
 					r1m1 = TimeDifference.between(t1, r1t4).in_minutes
 					r1d2 = r1d1.floor
-					r1h1 = r1h1-24*r1d2
+					r1h1 = r1h1 - 24 * r1d2
 					r1h2 = r1h1.floor
-					r1m1 = r1m1-60*r1h2-24*60*r1d2
+					r1m1 = r1m1 - 60 * r1h2 - 24 * 60 * r1d2
 					r1m2 = r1m1.floor
 					if r1t4.past?
 						raid1 = "```Raid 1:  In process or Completed"
@@ -32,11 +33,11 @@ module FelyneBot
 				if list == 'ready'
 					role = event.server.roles.find { |role| role.name == "RaidReady" }.id
 					raidready = event.server.members.select { |m| m.role?(role) }
-					i=0
+					i = 0
 					raidusers = ""
 					while i < raidready.length
 						raidusers << "**#{raidready[i].username}**\n"
-						i+=1
+						i += 1
 					end
 					event << raidusers
 					event << "#{raidready.length} users are ready to raid"
