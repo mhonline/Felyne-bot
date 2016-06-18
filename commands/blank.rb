0366dd40516a05174a33319b511d48737be92d4a
Reaver01 <reaver_01@gmx.com>
2016-06-19 01:00:10 +0200

    wiki search

--- commands/blank.rb
+++ commands/blank.rb
@@ -2,8 +2,10 @@ module FelyneBot
 	module Commands
 		module Blank
 			extend Discordrb::Commands::CommandContainer
-
+			command(:blank, description: '') do |event|
+				#put stuff here
 				nil
+			end
 		end
 	end
 end
---
+++ commands/wiki.rb
@@ -0,0 +1,14 @@
+module FelyneBot
+	module Commands
+		module Wiki
+			extend Discordrb::Commands::CommandContainer
+			command(:wiki, description: 'Searches the Wiki') do |event, list|
+				wiki = IO.readlines("bot/wiki")[0]
+				wiki = $wiki.split(",")
+				wiki.grep(/#{list}/).each { |x| links = "http://monsterhunteronline.in/#{x}\n" }
+				event << links
+				nil
+			end
+		end
+	end
+end
