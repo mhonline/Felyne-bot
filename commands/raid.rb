88d1ac6873a302d809b9f84619119d71fafb22c4
Reaver01 <reaver_01@gmx.com>
2016-06-18 21:07:36 +0200

    raid 1 fix

--- commands/raid.rb
+++ commands/raid.rb
@@ -3,8 +3,7 @@ module FelyneBot
 		module Raid
 			extend Discordrb::Commands::CommandContainer
 			command(:raid, description: 'Displays raid schedule.') do |event, list|
-				now = Time.now
-				now = now.to_i
+				t1 = Time.now
 				name1=IO.readlines("bot/raid1")[0]
 				name2=IO.readlines("bot/raid2")[0]
 				name3=IO.readlines("bot/raid3")[0]
@@ -14,12 +13,19 @@ module FelyneBot
 					raid1 = "```Raid 1:  Nothing set up"
 				else
 					name1=name1.gsub("\n","")
-					time1=IO.readlines("bot/raid1")[1].to_i
-					timediff1 = time1 - now - 3600
+					r1t4=IO.readlines("bot/raid1")[1]
+					r1d1 = TimeDifference.between(t1, r1t4).in_days
+					r1h1 = TimeDifference.between(t1, r1t4).in_hours
+					r1m1 = TimeDifference.between(t1, r1t4).in_minutes
+					r1d2 = r1d1.floor
+					r1h1 = r1h1-24*r1d2
+					r1h2 = r1h1.floor
+					r1m1 = r1m1-60*r1h2-24*60*r1d2
+					r1m2 = r1m1.floor
 					if time1 < now
 						raid1 = "```Raid 1:  In process or Completed"
 					else
-						raid1 = "```Raid 1:  #{name1} in #{Time.at(timediff1).strftime('%d Days %H hours %M minutes %S seconds')}"
+						raid1 = "```Raid 1:  #{name1} in #{d2} days #{h2} hours #{m2} minutes}"
 					end
 				end
 				if name2.to_s == ''
