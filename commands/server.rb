afab10ee7bc8aad97fbbc2505e7397bc4a1e4fb5
Reaver01 <reaver_01@gmx.com>
2016-06-18 14:25:34 +0200

    category does nothing

--- commands/server.rb
+++ commands/server.rb
@@ -2,7 +2,7 @@ module FelyneBot
 	module Commands
 		module Server
 			extend Discordrb::Commands::CommandContainer
-			command(:server, description: 'Displays a picture of the server most of us play on.', catagory: 'test') do |event|
+			command(:server, description: 'Displays a picture of the server most of us play on.') do |event|
 				event << "http://i.imgur.com/EHqV4Cy.jpg"
 				puts "#{event.timestamp}: #{event.user.name}: [server]"
 				nil
