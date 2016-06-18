f5a7bc706583131b4faa83ad1bc9d73f7f9529ed
Reaver01 <reaver_01@gmx.com>
2016-06-18 20:45:49 +0200

    integers

--- commands/raid1.rb
+++ commands/raid1.rb
@@ -3,10 +3,13 @@ module FelyneBot
 		module Raid1
 			extend Discordrb::Commands::CommandContainer
 			command(:raid1, description: 'Schedules Raid 1. (Name has to be a single word)', usage: '-raid1 <name> <hours> <minutes> | raid1 clear', permission_level: 1, permission_message: "Ask a Mod or Admin to set up a raid!") do |event, name, days, hours, minutes|
+				d = days.to_i
+				h = hours.to_i
+				m = minutes.to_i
 				t1 = Time.now
-				t2 = t1 + minutes*60
-				t3 = t2 + hours*60*60
-				t4 = t3 + days*24*60*60
+				t2 = t1 + m*60
+				t3 = t2 + h*60*60
+				t4 = t3 + d*24*60*60
 				if name == "clear"
 					File.write('bot/raid1', '')
 					event << "Raid 1 has been cleared"
