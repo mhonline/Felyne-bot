e81fc489bbdb80090f94c657137a1019d2813932
Reaver01 <reaver_01@gmx.com>
2016-06-18 20:56:11 +0200

    remove days too

--- commands/raid1.rb
+++ commands/raid1.rb
@@ -16,7 +16,7 @@ module FelyneBot
 				d2 = d1.floor
 				h1 = h1-24*d1
 				h2 = h1.floor
-				m1 = m1-60*h1
+				m1 = m1-60*h1-24*d1
 				m2 = m1.floor
 				if name == "clear"
 					File.write('bot/raid1', '')
