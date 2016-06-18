b34fcb07b14abe52ac710fc284ce5f869a975b90
Reaver01 <reaver_01@gmx.com>
2016-06-18 02:17:55 +0200

    test

--- commands/raid.rb
+++ commands/raid.rb
@@ -9,9 +9,9 @@ module FelyneBot
 				time1=IO.readlines("bot/raid1")[1].to_i
 				timediff1 = time1 - now - 3600
 				if time1 < now
-					raid1 = "Raid 1:  Started or Over"
+					raid1 = "```Raid 1:  Started or Over"
 				else
-					raid1 = "Raid 1:  #{name1} in #{Time.at(timediff1).strftime('%H hours %M minutes %S seconds')}"
+					raid1 = "```Raid 1:  #{name1} in #{Time.at(timediff1).strftime('%H hours %M minutes %S seconds')}```"
 				end
 			end
 		end
