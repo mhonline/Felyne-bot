9abf846ba9e1a8f01d108696c7e64f578e461894
Reaver01 <reaver_01@gmx.com>
2016-06-18 16:38:11 +0200

    errors

--- commands/announce.rb
+++ commands/announce.rb
@@ -4,9 +4,7 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(:announce, description: 'announces your text server-wide.', min_args: 1, permission_level: 1, usage: '-announce <text>') do |event, *text|
 				bot_profile = event.bot.profile.on(event.server)
-				text = shorten_text(text.join(' '), preview: event.server.preview,
-													original: event.server.original,
-													minlength: event.server.minlength)
+				text = text.join(' ')
 				event.server.text_channels.each do |channel|
 					next
 					channel.send_message("**#{event.author.username}**: #{text}")
