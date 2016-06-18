8a2bb1e1fe6e72b4862e6a36184a1e39351e8025
Reaver01 <reaver_01@gmx.com>
2016-06-18 20:53:28 +0200

    days hours minutes

--- commands/raid1.rb
+++ commands/raid1.rb
@@ -2,7 +2,7 @@ module FelyneBot
 	module Commands
 		module Raid1
 			extend Discordrb::Commands::CommandContainer
-			command(:raid1, description: 'Schedules Raid 1. (Name has to be a single word)', usage: '-raid1 <name> <hours> <minutes> | raid1 clear', permission_level: 1, permission_message: "Ask a Mod or Admin to set up a raid!") do |event, name, days, hours, minutes|
+			command(:raid1, description: 'Schedules Raid 1. (Name has to be a single word)', usage: '-raid1 <name> <days> <hours> <minutes> | raid1 clear', permission_level: 1, permission_message: "Ask a Mod or Admin to set up a raid!") do |event, name, days, hours, minutes|
 				d = days.to_i
 				h = hours.to_i
 				m = minutes.to_i
@@ -10,6 +10,14 @@ module FelyneBot
 				t2 = t1 + m*60
 				t3 = t2 + h*60*60
 				t4 = t3 + d*24*60*60
+				d1 = TimeDifference.between(t1, t4).in_days
+				h1 = TimeDifference.between(t1, t4).in_hours
+				m1 = TimeDifference.between(t1, t4).in_minutes
+				d2 = d1.floor
+				h1 = h1+24(d1-d2)
+				h2 = h1.floor
+				m1 = m1+60(h1-h2)
+				m2 = m1.floor
 				if name == "clear"
 					File.write('bot/raid1', '')
 					event << "Raid 1 has been cleared"
@@ -18,7 +26,7 @@ module FelyneBot
 					open('bot/raid1', 'a') { |f|
 						f.puts "\n#{t4}"
 					}
-					event << "#{TimeDifference.between(t1, t4).in_days} until raid"
+					event << "#{d2} days #{h2} hours #{m2} minutes until #{name} raid"
 				end
 			end
 		end
