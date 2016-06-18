98a436de752e1728eb7c3d0e77bc9a061d63e72c
Reaver01 <reaver_01@gmx.com>
2016-06-19 01:01:36 +0200

    command

--- commands/wiki.rb
+++ commands/wiki.rb
@@ -4,7 +4,7 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(:wiki, description: 'Searches the Wiki') do |event, list|
 				wiki = IO.readlines("bot/wiki")[0]
-				wiki = $wiki.split(",")
+				wiki = wiki.split(",")
 				wiki.grep(/#{list}/).each { |x| links = "http://monsterhunteronline.in/#{x}\n" }
 				event << links
 				nil
