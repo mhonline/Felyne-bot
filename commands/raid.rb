383b8aa37b79dd3c4ad4fb1e32af388650812b8d
Reaver01 <reaver_01@gmx.com>
2016-06-18 11:57:16 +0200

    raidlist

--- commands/raid.rb
+++ commands/raid.rb
@@ -73,7 +73,7 @@ module FelyneBot
 				event << "#{raid1}\n#{raid2}\n#{raid3}\n#{raid4}\n#{raid5}"
 				if list == 'ready'
 					role = event.server.roles.find { |role| role.name == "RaidReady" }
-					event << role.members
+					event << role.inspect
 				end
 			end
 		end
