7514dad98b5560d6014c7e2a372e3284ce5ec10d
Reaver01 <reaver_01@gmx.com>
2016-06-18 15:15:58 +0200

    keeping it random

--- commands/avatar.rb
+++ commands/avatar.rb
@@ -3,16 +3,7 @@ module FelyneBot
 		module Avatar
 			extend Discordrb::Commands::CommandContainer
 			command(:avatar, description: "Changes avatar randomly", permission_level: 1) do |event, text|
-				if text == ''
-					picture = 1 + rand(35)
-				else
-					if text.to_i.between?(0, 35)
-						picture = text
-					else
-						$bot.send_message(event.message.channel, "Invalid Option, randomizing!")
-						picture = 1 + rand(35)
-					end
-				end
+				picture = 1 + rand(35)
 				$bot.profile.avatar = File.open("/Felyne-bot/pic/#{picture}.jpg")
 				role = event.server.roles.find { |role| role.name == "Felyne" }
 				newcolor = '0x'
