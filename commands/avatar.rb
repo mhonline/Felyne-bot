adc977376f525aadff7e07356706ac1e5af11364
Reaver01 <reaver_01@gmx.com>
2016-06-18 11:25:36 +0200

    testing role writing

--- commands/avatar.rb
+++ commands/avatar.rb
@@ -5,7 +5,7 @@ module FelyneBot
 			command(:avatar, description: "Changes avatar randomly", permission_level: 1) do |event|
 				$bot.profile.avatar = File.open("/Felyne-bot/pic/#{1 + rand(35)}.jpg")
 				role = event.server.roles.find { |role| role.name == "Felyne" }
-				role.color = Discordrb::ColourRGB(0xffffff)
+				role.color = Discordrb::ColorRGB.new(0xffffff)
 				$bot.send_message(event.message.channel, "Changing Avatar!")
 				puts "Changing Avatar"
 				nil
