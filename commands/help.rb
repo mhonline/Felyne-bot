722210d0e2314e138130c4ef66e5289a53ee15ac
Reaver01 <reaver_01@gmx.com>
2016-06-19 21:31:36 +0200

    -help command

--- commands/help.rb
+++ commands/help.rb
@@ -2,7 +2,7 @@ module FelyneBot
 	module Commands
 		module Help
 			extend Discordrb::Commands::CommandContainer
-			command(:help, description: 'Shows all commands') do |event|
+			command(:help, description: 'Shows all commands') do |event, more|
 				admin = ['ping', 'kill']
 				database = ['adduser', 'userign', 'userguild', 'usertimezone', 'userremove', 'userlist', 'userfind']
 				helpful = ['reset', 'server', 'server47', 'maint', 'help']
@@ -10,20 +10,25 @@ module FelyneBot
 				raids = ['raid', 'raid1', 'raid2', 'raid3', 'raid4', 'raid5']
 				mods = ['mainsetup', 'rp', 'avatar', 'game', 'eval']
 
-				out = "```#Admin/Troubleshooting\n"
-				admin.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "\n#Admin/Troubleshooting\n"
-				database.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "\n#IGN/Guild/Timezone Database\n"
-				helpful.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "\n#Helpful Commands\n"
-				roles.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "\n#Raids\n"
-				raids.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "\n#Mod only\n"
-				mods.each { |x| out << "	#{$prefix}#{x}\n" }
-				out << "```"
-				event << out
+				if more == ''
+					out = "```#Admin/Troubleshooting\n"
+					admin.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Admin/Troubleshooting\n"
+					database.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#IGN/Guild/Timezone Database\n"
+					helpful.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Helpful Commands\n"
+					roles.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Raids\n"
+					raids.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Mod only\n"
+					mods.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "```"
+					event << out
+				else
+					seach = ':#{more}'
+					out = bot.commands[search].attributes[:description]
+				end
 				nil
 			end
 		end
