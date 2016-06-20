ec990c9f5a4d2a3cc428ca3076f65a06a6f73124
reaver01 <reaver_01@gmx.com>
2016-06-20 12:12:59 +0200

    Revert "Messed up order" All my changes were deleted by Alice...
    This reverts commit 6ddb13ea68d9d85a7b709be80b26cfb1c5ae2131.

--- commands/help.rb
+++ commands/help.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module FelyneBot
 	module Commands
 		module Help
@@ -40,4 +41,50 @@ module FelyneBot
 			end
 		end
 	end
+=======
+module FelyneBot
+	module Commands
+		module Help
+			extend Discordrb::Commands::CommandContainer
+			command(:help, description: 'Shows all commands') do |event, more|
+				admin = ['ping', 'kill']
+				database = ['adduser', 'userign', 'userguild', 'usertimezone', 'userremove', 'userlist', 'userfind']
+				helpful = ['reset', 'server', 'server47', 'maint', 'help']
+				roles = ['userraid', 'guild']
+				raids = ['raid', 'raid1', 'raid2', 'raid3', 'raid4', 'raid5']
+				mods = ['mainsetup', 'rp', 'avatar', 'game', 'eval']
+				if more == nil
+					more = "none"
+					out = "```#Admin/Troubleshooting\n"
+					admin.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Admin/Troubleshooting\n"
+					database.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#IGN/Guild/Timezone Database\n"
+					helpful.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Helpful Commands\n"
+					roles.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Raids\n"
+					raids.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\n#Mod only\n"
+					mods.each { |x| out << "	#{$prefix}#{x}\n" }
+					out << "\nTo get more info about a specific command type: #{$prefix}help <command>```"
+				else
+					search = more.to_sym
+					if $bot.commands[search] != nil
+						desc = $bot.commands[search].attributes[:description]
+						useage = $bot.commands[search].attributes[:usage]
+						out = "#{$prefix}#{more}: ``#{desc}``"
+						if useage != nil
+							out << "\nUseage: ``#{useage}``"
+						end
+					else
+						out = "No such command exists."
+					end
+				end
+				event << out
+				nil
+			end
+		end
+	end
+>>>>>>> parent of 6ddb13e... Messed up order
 end
