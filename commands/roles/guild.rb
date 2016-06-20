7e51316c668d9d34ad0261f94aae03846bedd41f
Reaver01 <reaver_01@gmx.com>
2016-06-20 17:40:31 +0200

    fixed guild command

--- commands/roles/guild.rb
+++ commands/roles/guild.rb
@@ -5,36 +5,36 @@ module FelyneBot
 			command(
 					:guild,
 					description: "Add user to guild",
-					usage: "guild <guildname/remove/list>",
+					usage: "guild <set/remove/list> <guildname",
 					min_args: 1,
-					max_args: 1,
-			) do |event, search|
+					max_args: 2,
+			) do |event, set, search|
 				guilds = ["Hipcheckers","Hyperlynx","Stygian","MHOInter","Discord","SG★Hunters","血盟狩猎团","月夜黑貓團","为卿负天下、","Abysswalker","thelegionnaires","GatotKaca","LaTaverne","MooGoo","TheHoldingPen","花落阴影"]
 				member = event.user.on(event.server)
 				guild = 0
-				if search == 'remove'
+				if set == 'remove'
 					guilds.each { |x| 
 						role = event.server.roles.find { |role| role.name == x }
 						member.remove_role(role)
 					}
 					event << "Removed from all guild server roles."
-				else
-					if search == 'list'
-						guilds.each { |x| event << x }
-					else
-						guilds.each { |x|
-							if search == x
-								role = event.server.roles.find { |role| role.name == search }
-								member.add_role(role)
-								guild = 1
-							end
-						}
-						if guild == 1
-							event << "Added to the #{search} guild server role"
-						else
-							event << "The requested guild server role does not exist.  If you would like to add your guild to the servers roles, please PM Reaver01"
+				end
+				if set == 'list'
+					guilds.each { |x| event << x }
+				end
+				if set == 'set'
+					guilds.each { |x|
+						if search == x
+							role = event.server.roles.find { |role| role.name == search }
+							member.add_role(role)
+							guild = 1
 						end
-					end
+					}
+				if guild == 1
+					event << "Added to the #{search} guild server role"
+				else
+					event << "The requested guild server role does not exist.  If you would like to add your guild to the servers roles, please PM Reaver01"
+				end
 				end
 				puts "#{event.timestamp}: #{event.user.name}: CMD: guild <#{search}>"
 				nil
