ef3b39eb8df36b2392dd8cc9aed88a5bcc1f7a7a
Reaver01 <reaver_01@gmx.com>
2016-06-20 17:29:57 +0200

    fixed itteration

--- commands/roles/guild.rb
+++ commands/roles/guild.rb
@@ -11,7 +11,7 @@ module FelyneBot
 			) do |event, search|
 				guilds = ["Hipcheckers","Hyperlynx","Stygian","MHOInter","Discord","SG★Hunters","血盟狩猎团","月夜黑貓團","为卿负天下、","Abysswalker","thelegionnaires","GatotKaca","LaTaverne","MooGoo","TheHoldingPen","花落阴影"]
 				member = event.user.on(event.server)
-				guildexists = 0
+				guild = 0
 				if search == 'remove'
 					guilds.each { |x| 
 						role = event.server.roles.find { |role| role.name == x }
@@ -19,16 +19,14 @@ module FelyneBot
 					}
 					event << "Removed from all guild server roles."
 				else
-					i=0
-					begin
-						if search == guilds[i]
+					guilds.each { |x|
+						if search == x
 							role = event.server.roles.find { |role| role.name == search }
 							member.add_role(role)
-							guildexists = 1
-							i+=1
+							guild = 1
 						end
-					end while guilds[i]!=nil
-					if guildexists == 1
+					}
+					if guild == 1
 						event << "Added to the #{search} guild server role"
 					else
 						event << "The requested guild server role does not exist.  If you would like to add your guild to the servers roles, please PM Reaver01"
