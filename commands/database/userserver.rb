5d9430d9ad47c4636c6cf6ee2364f7ee25711734
Reaver01 <reaver_01@gmx.com>
2016-06-20 22:27:04 +0200

    capital Server

--- commands/database/userserver.rb
+++ commands/database/userserver.rb
@@ -4,7 +4,7 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(:userserver, max_args: 1, min_args: 0, description: "Adds server for user to the database.", usage: "-userserver <server>") do |event, server=nil|
 				temp = $users.find_index {|s| s.id == event.user.id}
-				if  temp!=nil then $users[temp].addserver(server) end
+				if  temp!=nil then $users[temp].addServer(server) end
 				event << "Changed server to: #{server}"
 				puts "#{event.timestamp}: #{event.user.name}: [userserver] <#{server}>"
 				saveObj($users,"userbase/users")
