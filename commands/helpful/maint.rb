7b27dbf08384869842acec2e897b78f0412bac5e
Reaver01 <reaver_01@gmx.com>
2016-06-20 19:17:09 +0200

    start/end

--- commands/helpful/maint.rb
+++ commands/helpful/maint.rb
@@ -7,19 +7,40 @@ module FelyneBot
 					description: "Checks time left in game maintenance.",
 					useage: "maint"
 			) do |event|
-				t3 = IO.readlines("bot/maint")[0]
+				t3 = IO.readlines("bot/maintstart")[0]
 				t3 = Time.parse(t3.gsub("\n",""))
+				t4 = IO.readlines("bot/maintend")[0]
+				t4 = Time.parse(t3.gsub("\n",""))
 				t1 = Time.now
-				h1 = TimeDifference.between(t1, t3).in_hours
-				m1 = TimeDifference.between(t1, t3).in_minutes
-				h2 = h1.floor
-				m1 = m1-60*h2
-				m2 = m1.floor
-				if t3.past?
-					event << "Maintenance has ended! GO HUNTING!"
+				sh1 = TimeDifference.between(t1, t3).in_hours
+				sm1 = TimeDifference.between(t1, t3).in_minutes
+				sh2 = sh1.floor
+				sm1 = sm1-60*sh2
+				sm2 = sm1.floor
+				eh1 = TimeDifference.between(t1, t4).in_hours
+				em1 = TimeDifference.between(t1, t4).in_minutes
+				eh2 = eh1.floor
+				em1 = em1-60*eh2
+				em2 = em1.floor
+				if t3!=""
+					if t3.past?
+						started << "Maintenance has started!"
+					else
+						started << "#{sh2} hours #{sm2} minutes left until start of maintenance"
+					end
 				else
-					event << "#{h2} hours #{m2} minutes left until end of maintenance"
+					started << "Maintenance start time not set."
 				end
+				if t4!=""
+					if t4.past?
+						ended << "Maintenance has Ended! GO HUNTING!"
+					else
+						ended << "#{eh2} hours #{em2} minutes left until start of maintenance"
+					end
+				else
+					ended << "Maintenance end time not set."
+				end
+				event << "```#{started}\n#{ended}```"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: maint"
 				nil
 			end
