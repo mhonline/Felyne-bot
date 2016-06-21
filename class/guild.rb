966693dce687c30fceab18647325b4b76392ca29
Reaver01 <reaver_01@gmx.com>
2016-06-21 17:17:53 +0200

    fixed variable

--- class/guild.rb
+++ class/guild.rb
@@ -2,11 +2,12 @@ class Guilds
 	def initialize(role, guildname, array, channel, b)
 		@guild_role=role
 		@guild_name=guildname
+		
 		if array.empty?
 			array.push(self)
 			b.send_message(channel, "**#{guildname}** not found in (empty) database, added. Guild: **#{guildname}**")
 		else
-			if array.any?{|a| a.guildname == guildname}
+			if array.any?{|a| a.guild_name == guild_name}
 				b.send_message(channel, "**#{guildname}** found in database, skipped.")
 			else
 				array.push(self)
