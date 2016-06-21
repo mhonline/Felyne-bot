046f1c09e75b98171bd92f5c7be2db564b54400d
Reaver01 <reaver_01@gmx.com>
2016-06-21 21:42:46 +0200

    fixed announce command.
    Best if this is never used.

--- commands/mods/announce.rb
+++ commands/mods/announce.rb
@@ -4,17 +4,14 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(
 					:announce,
-					description: "announces your text server-wide.",
+					description: "announces your text server-wide. (Probably shouldn't use this...)",
 					usage: "announce <text>",
 					min_args: 1,
-					permission_level: 1,
+					permission_level: 800,
 			) do |event, *text|
 				bot_profile = event.bot.profile.on(event.server)
 				text = text.join(' ')
-				event.server.text_channels.each do |channel|
-					next
-					channel.send_message("**#{event.author.username}**: #{text}")
-				end
+				event.server.text_channels.each { |x| event.bot.send_message(x, text) }
 				puts "#{event.timestamp}: #{event.user.name}: CMD: announce"
 				nil
 			end
