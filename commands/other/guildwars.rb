fa44ec86cffa6a36a014e5a090659d7aef8da181
Reaver01 <reaver_01@gmx.com>
2016-06-22 16:37:20 +0200

    fix array

--- commands/other/guildwars.rb
+++ commands/other/guildwars.rb
@@ -6,13 +6,13 @@ module FelyneBot
 					:guildwars,
 			) do |event|
 				guilds = []
-				(1..$guilds.length).each { |i| guilds.push(["guild_name": $guilds[i-1].guild_name, "members": 0]) }
+				(1..$guilds.length).each { |i| guilds.push([$guilds[i-1].guild_name, 0]) }
 				member = event.user.on(event.server)
 				(1..guilds.length).each { |i| 
-					role = event.server.roles.find { |role| role.name == guilds.guild_name[i-1] }.id
+					role = event.server.roles.find { |role| role.name == guilds.[i-1][0] }.id
 					members = event.server.members.select { |m| m.role?(role) }
-					guilds.members[i-1] = members.length
-					event << "Guild: #{guilds.guild_name} | Members: #{guilds.members}"
+					guilds[i-1][1] = members.length
+					event << "Guild: #{guilds[i-1][0]} | Members: #{guilds[i-1][1]}"
 				}		
 				nil
 			end
