b62bc6ed121501a39c5c0fcdb86f8219dd7d7fbd
Reaver01 <reaver_01@gmx.com>
2016-06-22 16:39:44 +0200

    removed .

--- commands/other/guildwars.rb
+++ commands/other/guildwars.rb
@@ -9,9 +9,9 @@ module FelyneBot
 				(1..$guilds.length).each { |i| guilds.push([$guilds[i-1].guild_name, 0]) }
 				member = event.user.on(event.server)
 				(1..guilds.length).each { |i| 
-					role = event.server.roles.find { |role| role.name == guilds.[i-1][0] }.id
+					role = event.server.roles.find { |role| role.name == guilds[i-1][0] }.id
 					members = event.server.members.select { |m| m.role?(role) }
-					guilds[(i-1)][1] = members.length
+					guilds[i-1][1] = members.length
 					event << "Guild: #{guilds[i-1][0]} | Members: #{guilds[i-1][1]}"
 				}		
 				nil
