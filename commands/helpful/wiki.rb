b100f7a81b674a0088aeebbd8439746219d16d65
Reaver01 <reaver_01@gmx.com>
2016-06-22 18:58:34 +0200

    fix if wiki is by itself

--- commands/helpful/wiki.rb
+++ commands/helpful/wiki.rb
@@ -8,29 +8,33 @@ module FelyneBot
 					useage: "wiki <search>"
 			) do |event, list, *search|
 				search = search.join(' ')
-				if list == 'mats'
-					event << "<http://monsterhunteronline.in/materials/?search=#{search}>"
+				if list == nil
+					event << "<http://monsterhunteronline.in>"
 				else
-					if list == 'skill'
-						event << "<http://monsterhunteronline.in/skills/?search=#{search}>"
+					if list == 'mats'
+						event << "<http://monsterhunteronline.in/materials/?search=#{search}>"
 					else
-						if list == 'armor'
-							event << "<http://monsterhunteronline.in/armor/?search=#{search}>"
+						if list == 'skill'
+							event << "<http://monsterhunteronline.in/skills/?search=#{search}>"
 						else
-							if list == 'monsters'
-								event << "<http://monsterhunteronline.in/monsters>"
+							if list == 'armor'
+								event << "<http://monsterhunteronline.in/armor/?search=#{search}>"
 							else
-								wiki = IO.readlines("bot/wiki")[0]
-								wiki = wiki.split(",")
-								links = ""
-								wiki.grep(/#{list}/).each { |x| links << "<http://monsterhunteronline.in/#{x}> \n" }
-								if links.length > 2000
-									event << "Output has too many characters. Please be more specific in your search."
+								if list == 'monsters'
+									event << "<http://monsterhunteronline.in/monsters>"
 								else
-									if links.length < 1
-										event << "I wasn't able to dig up any results.  Please try something else!"
+									wiki = IO.readlines("bot/wiki")[0]
+									wiki = wiki.split(",")
+									links = ""
+									wiki.grep(/#{list}/).each { |x| links << "<http://monsterhunteronline.in/#{x}> \n" }
+									if links.length > 2000
+										event << "Output has too many characters. Please be more specific in your search."
 									else
-										event << links
+										if links.length < 1
+											event << "I wasn't able to dig up any results.  Please try something else!"
+										else
+											event << links
+										end
 									end
 								end
 							end
