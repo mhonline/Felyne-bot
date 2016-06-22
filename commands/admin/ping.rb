a5b9febcdc5d37b69265aa2093cd18aef6813846
reaver01 <reaver_01@gmx.com>
2016-06-22 14:09:52 +0200

    makepingcommandgreatagain

--- commands/admin/ping.rb
+++ commands/admin/ping.rb
@@ -7,7 +7,9 @@ module FelyneBot
 					description: "Responds with response time",
 					useage: "ping"
 			) do |event|
-				event <<  "Pong! : #{((Time.now - event.timestamp) * 1000).to_i}ms."
+				x = ["a","e","i","o","u"]
+				y = ["B","C","D","F","G","H","J","K","L","M","N","P","R","S","T","V","W","X","Z"]
+				event <<  "#{y[rand(0..y.length-1)]}#{x[rand(0..x.length-1)]}ng! : #{((Time.now - event.timestamp) * 1000).to_i}ms."
 				puts "#{event.timestamp}: #{event.user.name}: CMD: ping"
 				nil
 			end
