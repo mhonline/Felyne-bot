f956d86aff242a5c435c7568aa27efa7aa1d08e2
reaver01 <reaver_01@gmx.com>
2016-06-22 13:06:57 +0200

    Dice roll comman

--- commands/other/roll.rb
+++ commands/other/roll.rb
@@ -4,7 +4,10 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(
 					:roll,
-			) do |event, number=6|
+			) do |event, number|
+				if number == nil
+					number = 6
+				end
 				event << "A D#{number} is rolled: #{rand(1..number)}"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: roll"
 				nil
