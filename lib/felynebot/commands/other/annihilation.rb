2a7aaf9cdb9fbe62f86713233e09eed20ef3404f
Reaver01 <reaver_01@gmx.com>
2016-06-23 19:53:03 +0200

    proper percent

--- lib/felynebot/commands/other/annihilation.rb
+++ lib/felynebot/commands/other/annihilation.rb
@@ -7,7 +7,7 @@ module FelyneBot
 			) do |event, number|
 				gcount = IO.readlines("bot/gcount")[0]
 				gcount = gcount.to_i
-				a = gcount/1000
+				a = (gcount / 10.00)
 				event << "Annihilation Program is currently #{a}% loaded."
 				puts "#{event.timestamp}: #{event.user.name}: CMD: annil"
 				nil
