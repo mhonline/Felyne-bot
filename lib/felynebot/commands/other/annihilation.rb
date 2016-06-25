29e01cd1d108d7702b740c2502a7f72a0af88538
Reaver01 <reaver_01@gmx.com>
2016-06-25 15:05:46 +0200

    variable

--- lib/felynebot/commands/other/annihilation.rb
+++ lib/felynebot/commands/other/annihilation.rb
@@ -9,7 +9,7 @@ module FelyneBot
 				gcount = IO.readlines("bot/gcount")[0]
 				gcount = gcount.to_i
 				ecount = IO.readlines("bot/ecount")[0]
-				ecount = gcount.to_i
+				ecount = ecount.to_i
 				a = (gcount / 10.00)
 				b = (ecount / 10.00)
 				event << "Annihilation Program is currently #{a}% loaded."
