92a0bb1ad7dc3b2147958314010a861a30b90e12
reaver01 <reaver_01@gmx.com>
2016-06-27 09:18:23 +0200

    test addmod

--- lib/felynebot/commands/admin/addmod.rb
+++ lib/felynebot/commands/admin/addmod.rb
@@ -10,20 +10,14 @@ module FelyneBot
 					min_args: 1,
 					max_args: 2,
 					permission_message: "I'm sorry Dave, I cannot do that.",
-			) do |event, uname, level|
+			) do |event, uname, level=1|
 				permarray = []
 				permarray = loadArr(permarray,"userbase/perm")
 				$userid = $bot.parse_mention(uname).id
 				$username = $bot.parse_mention(uname).name
-				if $userid != nil
-					if permarray.include? $userid
-						event << "User permissions found... Updating permissions."
-					else
-						event << "User permissions not found... Adding permissions."
-					end
-				else
-					event << "Invalid user."
-				end
+				event << $userid
+				event << $username
+				event << level
 				puts "#{event.timestamp}: #{event.user.name}: CMD: addmod"
 				exit
 				nil
