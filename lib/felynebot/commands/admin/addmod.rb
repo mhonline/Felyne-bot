0fc92c2100a60c2a5fb8bac91c653a84424e0ca3
reaver01 <reaver_01@gmx.com>
2016-06-27 10:29:19 +0200

    test addmod

--- lib/felynebot/commands/admin/addmod.rb
+++ lib/felynebot/commands/admin/addmod.rb
@@ -4,13 +4,13 @@ module FelyneBot
 			extend Discordrb::Commands::CommandContainer
 			command(
 					:addmod,
-					description: "Kills felyne",
-					useage: "addmod <@name> <level>",
+					description: "Adds mod permissions to Felyne.",
+					useage: "addmod <@name> <level> <force>",
 					permission_level: 800,
 					min_args: 1,
-					max_args: 2,
+					max_args: 3,
 					permission_message: "I'm sorry Dave, I cannot do that.",
-			) do |event, uname, level|
+			) do |event, uname, level=1, force="no"|
 				permarray = []
 				permarray = loadArr(permarray,"userbase/perm")
 				if $bot.parse_mention(uname) !=nil
@@ -20,7 +20,14 @@ module FelyneBot
 						event << "User permissions found... Updating permissions."
 						hash = Hash[permarray.map.with_index.to_a]
 						i = hash[userid].to_i
-						curlevel = permarray[i+2]
+						curlevel = permarray[i+1]
+						if curlevel > level
+							if force == "yes"
+								event << "Forcing permission change to lower level."
+							else
+								event << "User permissions level is admin or higher, you must force permissions change to set lower."
+							end
+						end
 						event << "Current permission level: #{curlevel}"
 					else
 						event << "User permissions not found... Adding permissions."
