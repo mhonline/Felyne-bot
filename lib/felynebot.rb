eb97564f5c52cca1447bea0654159c4697462286
Reaver01 <reaver_01@gmx.com>
2016-07-04 04:19:55 +0800

    test cross channel messaging

--- lib/felynebot.rb
+++ lib/felynebot.rb
@@ -119,76 +119,13 @@ module FelyneBot
 	$bot.message(containing: "┬─┬﻿ ノ( ゜-゜ノ)") { |event|
 		event << "(╯°□°）╯︵ ┻━┻"
 	}
-#	$bot.message(containing: "g") { |event|
-#		if File.file?("bot/gcount")
-#			gcount = IO.readlines("bot/gcount")[0]
-#		else
-#			gcount=0
-#		end
-#		gcount = gcount.to_i
-#		gcount += 1
-#		File.write('bot/gcount', gcount)
-#		#puts "Number of g's in chat: #{gcount}"
-#		if gcount == 100 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 10%");puts gcount end
-#		if gcount == 200 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 20%");puts gcount end
-#		if gcount == 300 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 30%");puts gcount end
-#		if gcount == 400 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 40%");puts gcount end
-#		if gcount == 500 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 50%");puts gcount end
-#		if gcount == 600 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 60%");puts gcount end
-#		if gcount == 700 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 70%");puts gcount end
-#		if gcount == 800 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 80%");puts gcount end
-#		if gcount == 900 then $bot.send_message(122526505606709257, "Annihilation Program Loading... 90%");puts gcount end
-#
-#		if gcount == 1000
-#			File.write('bot/gcount', "0")
-#			hexlist = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"]
-#			$bot.send_message(122526505606709257, "Annihilation Program Loading... 100%")
-#			$bot.send_message(122526505606709257, "Annihilation Commencing")
-#			puts gcount
-#			guilds = []
-#			(1..$guilds.length).each { |i| guilds.push($guilds[i-1].guild_name) }
-#			guilds.each { |x|
-#				role = event.bot.server(122526505606709257).roles.find { |role| role.name == x }
-#				newcolor = "0x#{hexlist[rand(0..15)]}#{hexlist[rand(0..15)]}#{hexlist[rand(0..15)]}#{hexlist[rand(0..15)]}#{hexlist[rand(0..15)]}#{hexlist[rand(0..15)]}".hex
-#				role.color = Discordrb::ColorRGB.new(newcolor)
-#				role.hoist = true
-#			}
-#			$bot.send_message(122526505606709257, "Annihilation Complete... Reseting")
-#		end
-#	}
-#	$bot.message(containing: "e") { |event|
-#		if File.file?("bot/ecount")
-#			ecount = IO.readlines("bot/ecount")[0]
-#		else
-#			ecount = 0
-#		end
-#		ecount = ecount.to_i
-#		ecount += 1
-#		File.write('bot/ecount', ecount)
-#		if ecount == 2000
-#			File.write('bot/ecount', "0")
-#			File.write('bot/gcount', "0")
-#			$bot.send_message(122526505606709257, "Annihilation Program has been interrupted... Reseting")
-#		end
-#	}
-	#Code for saving the last message of someone
-#	$bot.message() { |event|
-#		if $mess.empty?
-#			Mess.new(event.user.id, event.user.name, event.timestamp, event.message.to_s, $mess)
-#			saveObj($mess,"userbase/mess")
-#		else
-#			if $mess.any?{|a| a.id == event.user.id}
-#				pos=$mess.find_index {|item| item.id == event.user.id}
-#				$mess[pos].s_name(event.user.name)
-#				$mess[pos].s_time(event.timestamp)
-#				$mess[pos].s_mess(event.message.to_s)
-#				saveObj($mess,"userbase/mess")
-#			else
-#				Mess.new(event.user.id, event.user.name, event.timestamp, event.message.to_s, $mess)
-#				saveObj($mess,"userbase/mess")
-#			end
-#		end
-#	}
+	$bot.message { |event|
+		if(event.channel.id == 197762918337478656)
+			$bot.send_message(189667083586502656, "#{event.message.timestamp} in #{event.message.channel.name}: #{event.message.content.gsub(/<@!?(\d+)>/){ |m| event.server.member($1, false).name }}")
+		end
+	}
+#website: 197762918337478656
+#betabot: 189667083586502656
 
 	puts "Commands Loaded"
 	$bot.debug = false
