48d1a6bcc24d3057032849ab0e237a3e3feac350
Reaver01 <reaver_01@gmx.com>
2016-07-28 23:49:51 +0200

    Update server.rb

--- lib/felynebot/commands/helpful/server.rb
+++ lib/felynebot/commands/helpful/server.rb
@@ -7,7 +7,7 @@ module FelyneBot
 					description: "Displays a picture of the server most of us play on.",
 					useage: "server"
 			) do |event|
-				event.respond "http://i.imgur.com/EHqV4Cy.jpg"
+				event.respond "https://i.gyazo.com/120e19545d6b56df935fd24617d47001.jpg"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: server"
 				nil
 			end
