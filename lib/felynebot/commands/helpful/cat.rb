5d8d85d5f945c2fda329556f30f0a93c310f1beb
Reaver01 <reaver_01@gmx.com>
2016-09-19 07:45:56 +0200

    Update and rename cat to cat.rb

--- lib/felynebot/commands/helpful/cat.rb
+++ lib/felynebot/commands/helpful/cat.rb
@@ -9,7 +9,7 @@ module FelyneBot
 					useage: "cat"
 			) do |event|
 				event.respond "<https://drive.google.com/open?id=1Ynk9jhza6R9enhmNw2-8pUwtuHDfkPx3cm98ZYW3oA0>"
-				puts "#{event.timestamp}: #{event.user.name}: CMD: catskill"
+				puts "#{event.timestamp}: #{event.user.name}: CMD: cat"
 				nil
 			end
 		end
