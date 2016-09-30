f4c6f2eb0a7813c74ce21f7077bd77aba21a45bf
Reaver01 <reaver_01@gmx.com>
2016-09-30 11:37:32 +0200

    Update wiki.rb

--- lib/felynebot/commands/helpful/wiki.rb
+++ lib/felynebot/commands/helpful/wiki.rb
@@ -24,16 +24,20 @@ module FelyneBot
 								if list == 'monsters'
 									event.respond "<http://monsterhunteronline.in/monsters>"
 								else
-									wiki = $wikilinks
-									links = ""
-									wiki.grep(/#{list}/).each { |x| links << "<http://monsterhunteronline.in/#{x}> \n" }
-									if links.length > 2000
-										event.respond "Output has too many characters. Please be more specific in your search."
+									if list == 'random'
+										event.respond "<http://monsterhunteronline.in/#{$wikilinks[rand(0..($wikilinks.length-1))]}>"
 									else
-										if links.length < 1
-											event.respond "I wasn't able to dig up any results.  Please try something else!"
+										wiki = $wikilinks
+										links = ""
+										wiki.grep(/#{list}/).each { |x| links << "<http://monsterhunteronline.in/#{x}> \n" }
+										if links.length > 2000
+											event.respond "Output has too many characters. Please be more specific in your search."
 										else
-											event.respond links
+											if links.length < 1
+												event.respond "I wasn't able to dig up any results.  Please try something else!"
+											else
+												event.respond links
+											end
 										end
 									end
 								end
