93d30234e43926d43ed8361ce4e708bbfa15d692
Reaver01 <reaver_01@gmx.com>
2016-11-14 18:33:20 +0100

    fix

--- lib/felynebot.rb
+++ lib/felynebot.rb
@@ -12,12 +12,6 @@ module FelyneBot
 	$topnews = $topnews.chars.select(&:valid_encoding?).join
 	$topnewsdate = $topnews[/#{"<li><span>"}(.*?)#{"</span>"}/m, 1]
 	$topnewslink = $topnews[/#{"a><ahref=\""}(.*?)#{"\"target"}/m, 1]
-	if !$news.include? $topnewsdate + " | " + $topnewslink
-		$news.push($topnewsdate + " | " + $topnewslink)
-		$bot.send_message(126766276038230016, "http://mho.qq.com" + $topnewslink)
-		$news = $news.join(",")
-		File.write("bot/oldnews", $news)
-	end
 	$users = []
 	$guilds = []
 	$wikilinks = ["armor", "cats", "crafting", "food", "gathering", "hunter-set", "hunter-set/#blademaster", "hunter-set/#gunner", "hunter-set/#list", "hunting-groups", "install", "jewelry", "materials", "monsters", "monsters/akura-jebia", "monsters/akura-vashimu", "monsters/azure-rathalos", "monsters/baelidae", "monsters/basarios", "monsters/blangonga", "monsters/blue-yian-kut-ku", "monsters/bulldrome", "monsters/burning-tartaronis", "monsters/caeserber", "monsters/cephadrome", "monsters/chameleos", "monsters/chramine", "monsters/conflagration-rathian", "monsters/congalala", "monsters/crystal-basarios", "monsters/daimyo-hermitaur", "monsters/diablos", "monsters/doom-estrellian", "monsters/dread-baelidae", "monsters/elemental-merphistophelin", "monsters/estrellian", "monsters/flame-blangonga", "monsters/gendrome", "monsters/ghost-caeserber", "monsters/giadrome", "monsters/gold-congalala", "monsters/gold-hypnocatrice", "monsters/gravios", "monsters/guren-shen-gaoren", "monsters/gypceros", "monsters/hypnocatrice", "monsters/ice-chramine", "monsters/imag", "monsters/iodrome", "monsters/khezu", "monsters/kushala-daora", "monsters/lavasioth", "monsters/lightenna", "monsters/merphistophelin", "monsters/monoblos", "monsters/one-eared-yian-garuga", "monsters/pink-rathian", "monsters/plesioth", "monsters/purple-gypceros", "monsters/purple-slicemargl", "monsters/rajang", "monsters/rathalos", "monsters/rathian", "monsters/red-khezu", "monsters/sandstone-basarios", "monsters/shattered-monoblos", "monsters/shen-gaoren", "monsters/shogun-ceanataur", "monsters/silver-hypnocatrice", "monsters/slicemargl", "monsters/swordmaster-shogun-ceanataur", "monsters/tartaronis", "monsters/tepekki-shen-gaoren", "monsters/tigrex", "monsters/velocidrome", "monsters/white-monoblos", "monsters/yellow-caeserber", "monsters/yian-garuga", "monsters/yian-kut-ku", "mosaics", "npcs", "quests", "quests/#arena", "quests/#bounty", "quests/#elites", "quests/#monsters", "quests/#raids", "quests/#tickets", "quests/#unstable", "skills", "talismans", "talismans/table", "translation", "vip", "weapons", "weapons/bow", "weapons/bowgun", "weapons/dual-blades", "weapons/greatsword", "weapons/gunlance", "weapons/hammer", "weapons/hunting-horn", "weapons/lance", "weapons/longsword", "weapons/sword-and-shield"]
@@ -171,6 +165,13 @@ module FelyneBot
 	else
 		$bot.game = 0
 	end
+	if !$news.include? $topnewsdate + " | " + $topnewslink
+		$news.push($topnewsdate + " | " + $topnewslink)
+		$bot.send_message(126766276038230016, "http://mho.qq.com" + $topnewslink)
+		$news = $news.join(",")
+		File.write("bot/oldnews", $news)
+	end
+	puts '#newsupdate updated.'
 	puts 'Sync Confirmed.'
 	puts 'SKYNET ONLINE'
 	$bot.sync
