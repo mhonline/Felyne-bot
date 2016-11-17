c87ec931eaddea188b68d3789d072269c05d9a21
Reaver01 <reaver_01@gmx.com>
2016-11-17 23:32:25 +0100

    fix event

--- lib/felynebot/commands/other/hosting.rb
+++ lib/felynebot/commands/other/hosting.rb
@@ -5,7 +5,7 @@ module FelyneBot
 			command(
 					:hosting,
 					bucket: :delay10
-			) do event
+			) do |event|
 				event.respond "Felyne is hosted on DigitalOcean Servers.\nHost your own bot/app with $10 credit using my referral link: <https://m.do.co/c/71922c060e60>\nThis will help me keep Felyne live as long as you keep using their hosting after the first $10 credit :D"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: hosting"
 				nil
