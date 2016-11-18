ae664c8ea88a63fd856fe16fd3b2ab7a5cccc393
Reaver01 <jawmac@gmail.com>
2016-11-18 08:58:21 +0100

    Update rp.rb

--- lib/felynebot/commands/mods/rp.rb
+++ lib/felynebot/commands/mods/rp.rb
@@ -6,6 +6,7 @@ module FelyneBot
 					:rp,
 					description: "Nothing to see here.",
 					useage: "rp <text>",
+					help_available: false,
 					permission_level: 2,
 					permission_message: "I'm sorry Dave, I cannot do that."
 			) do |event, *phrase|
