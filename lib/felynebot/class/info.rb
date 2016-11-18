238f1e99c9c13a3855f5e73d80c3be61a6593da4
Reaver01 <jawmac@gmail.com>
2016-11-18 13:30:54 +0100

    Update info.rb

--- lib/felynebot/class/info.rb
+++ lib/felynebot/class/info.rb
@@ -4,21 +4,24 @@ def prompt(*args)
 end
 
 class Info
-	def initialize(loc1,loc2)
-		@i_id=nil
-		@i_token=nil
+	def initialize(loc1,loc2) #loc3
+		@i_id = nil #i_cid
+		@i_token = nil
+#		@i_aid=nil
 
+
+	#Open file for Client ID/Get ID
 		if File.exist?(loc1)
 			f = File.open(loc1,"r")
 			token = f.read
-			@i_token=token
+			@i_token = token
 			f.close
-			puts "Token Loaded"
+			puts "Token Loaded!"
 		else
 			puts 'No file found for the Token String! Please input your token.'
 			token = prompt "Token: "
-			@i_token=token
-			q= prompt "Store this for next time? y/n: "
+			@i_token = token
+			q = prompt "Store this for next time? y/n: "
 		if q[0] == "y"
 			if File.exist?(loc1)
 				f = File.open(loc1,"w")
@@ -30,17 +33,18 @@ class Info
 			f.close
 		end
 	end
-#Open file for ID/Get ID
+
+	#Open file for Client ID/Get ID
 	if File.exist?(loc2)
 		f = File.open(loc2,"r")
-		id = f.read
-		@i_id=id
+		id = f.read #cid = f.read
+		@i_id = id #i_cid = cid
 		f.close
-		puts "ID Loaded"
+		puts "Client ID Loaded!"
 	else
 		puts 'No file found for the ID String! Please input the ID.'
-		id = prompt "Client/Client ID: "
-		@i_id=id
+		id = prompt "Client/Client ID: " #cid = prompt "Client/Client ID:"
+		@i_id = id #i_cid = cid
 
 		q= prompt "Store this for next time? y/n: "
 		if q[0] == "y"
@@ -50,16 +54,47 @@ class Info
 				puts "Creating new file [#{loc2}]"
 				f = File.new(loc2,"w")
 			end
-				f.write(id)
+				f.write(id) #f.write(cid)
 				f.close
 			end
 		end
-	end
+	end	
 
-	def id
-		@i_id
+	#Open file for Application ID/Get ID
+#	if File.exist?(loc3)
+#		f = File.open(loc3,"r")
+#		aid = f.read
+#		@i_aid = aid
+#		f.close
+#		puts "Application ID Loaded!"
+#	else
+#		puts 'No file found for the ID String! Please input the ID.'
+#		aid = prompt "Aplication/Application ID: "
+#		@i_aid = aid
+#
+#		q = prompt "Store this for next time? y/n: "
+#		if q[0] == "y"
+#			if File.exist?(loc3)
+#				f = File.open(loc3,"w")
+#			else
+#				puts "Creating new file [#{loc3}]"
+#				f = File.new(loc3,"w")
+#			end
+#				f.write(aid)
+#				f.close
+#			end
+#		end
+#	end
+
+
+	def id #cid
+		@i_id #@i_cid
 	end
-#Defining the user name variable (call it with user.name)
+	
+#	def aid
+#		@i_aid
+#	end
+	
 	def token
 		@i_token
 	end
