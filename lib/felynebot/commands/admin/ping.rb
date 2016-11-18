b2c37bb1569e4066000bf177bded4a1231ad8880
Reaver01 <jawmac@gmail.com>
2016-11-18 09:17:34 +0100

    Update ping.rb

--- lib/felynebot/commands/admin/ping.rb
+++ lib/felynebot/commands/admin/ping.rb
@@ -7,7 +7,7 @@ module FelyneBot
 					description: "Responds with response time",
 					useage: "ping"
 			) do |event|
-				event.respond  ":ping_pong:Pong! : #{((Time.now - event.timestamp) * 1000).to_i.abs}ms:ping_pong:"
+				event.respond  ":ping_pong: Pong! : #{((Time.now - event.timestamp) * 1000).to_i.abs}ms :ping_pong:"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: ping"
 				nil
 			end
