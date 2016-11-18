da29179bcec6cb42813517cb6e87bb4fcbed389d
Reaver01 <jawmac@gmail.com>
2016-11-18 13:52:28 +0100

    Update felynebot.rb

--- lib/felynebot.rb
+++ lib/felynebot.rb
@@ -6,7 +6,7 @@ module FelyneBot
 	currentnews = open('http://mho.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
 	currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
 	currentnews.delete! "\s"
-	File.write('bot/newnews', $currentnews)
+	File.write('bot/newnews', currentnews)
 	puts "News gathered!"
 	
 	#Create Global Variables
