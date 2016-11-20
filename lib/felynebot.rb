879e472dbbd75b8a77087ac885f8ef83cce3a3d9
Reaver01 <reaver_01@gmx.com>
2016-11-20 22:43:08 +0800

    fix cron jobs

--- lib/felynebot.rb
+++ lib/felynebot.rb
@@ -229,17 +229,18 @@ module FelyneBot
 	end
 	puts '$news Posted!'
 	
-	scheduler.cron '0 19 * * *' {
+	scheduler.cron '0 19 * * *' do
 		$bot.send_message(122526505606709257, "Daily gift/ticket reset just happened!\nDon't forget to collect your rewards!")
 		$bot.send_message(125859373393117184, "Daily gift/ticket reset just happened!\nDon't forget to collect your rewards!")
-	}
-	scheduler.cron '0 19 * * 4' {
+	end
+	scheduler.cron '0 19 * * 4' do
 		$bot.send_message(122526505606709257, "Weekly gift/ticket reset just happened!\nYour extra ticket storage has been refilled!\nAny weekly events such as Astrolab have been reset!\nDon't forget to collect your rewards!")
 		$bot.send_message(125859373393117184, "Weekly gift/ticket reset just happened!\nYour extra ticket storage has been refilled!\nAny weekly events such as Astrolab have been reset!\nDon't forget to collect your rewards!")
-	}
-	scheduler.cron '5 */3 * * *' {
+	end
+	scheduler.cron '5 */3 * * *' do
 		$bot.stop
-	}
+	end
+
 
 	if File.exist?("bot/pmcron")
 
