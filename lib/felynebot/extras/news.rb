c44791a523f7ab68d23ebcb35ba05c39991be3f8
Reaver01 <reaver_01@gmx.com>
2016-11-23 22:03:52 +0800

    test net/http

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,12 +1,14 @@
 def news_pull
 	url = 'http://mh0.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml'
-	if open(url)
-		$currentnews = open(url).read
-		$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
-		$currentnews.delete! "\s"
-		File.write('bot/newnews', $currentnews)
-		puts "News gathered!"
-	else
-		puts "Can't Connect to news!"
-	end
+	req = Net::HTTP.new(url.host, url.port)
+	res = req.request_head(url.path)
+	puts res
+	#	$currentnews = open(url).read
+	#	$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
+	#	$currentnews.delete! "\s"
+	#	File.write('bot/newnews', $currentnews)
+	#	puts "News gathered!"
+	
+	#	puts "Can't Connect to news!"
+	
 end
