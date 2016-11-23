d7405164ef145a4096ca99975ee864eea2db85fd
Reaver01 <reaver_01@gmx.com>
2016-11-23 22:09:18 +0800

    rescue

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,7 +1,10 @@
 def news_pull
 	url = URI.parse("http://mh0.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml")
 	req = Net::HTTP.new(url.host, url.port)
-	res = req.request_head(url.path)
+	begin
+		res = req.request_head(url.path)
+	rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+	end
 	puts res
 	#	$currentnews = open(url).read
 	#	$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
