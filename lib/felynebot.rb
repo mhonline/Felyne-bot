becc43ddbeb257e7083b9902b226fe83cabd9556
Reaver01 <reaver_01@gmx.com>
2016-11-23 22:22:14 +0800

    move cron job scheduler to own file

--- lib/felynebot.rb
+++ lib/felynebot.rb
@@ -1,5 +1,6 @@
 module FelyneBot
 	require_relative 'felynebot/extras/news'
+	require_relative 'felynebot/extras/cron'
 	#Pull $news from mho.qq.com and store to file
 	news_pull
 	
@@ -161,50 +162,7 @@ module FelyneBot
 	news_post
 
 	#scedule cron jobs
-	scheduler.cron '0 19 * * 1,2,3,5,6,7' do
-		$bot.send_message(122526505606709257, "Daily gift/ticket reset just happened!\nDon't forget to collect your rewards!")
-		$bot.send_message(125859373393117184, "Daily gift/ticket reset just happened!\nDon't forget to collect your rewards!")
-		if File.file?("bot/dailypm")
-			dailypm = loadArr(dailypm,"bot/dailypm")
-		else
-			dailypm = []
-		end
-		dailypm.each { |x| $bot.user(x).pm("Daily gift/ticket reset just happened!\nDon't forget to collect your rewards!")}
-	end
-	scheduler.cron '0 19 * * 4' do
-		$bot.send_message(122526505606709257, "Weekly gift/ticket reset just happened!\nYour extra ticket storage has been refilled!\nAny weekly events such as Astrolab have been reset!\nDon't forget to collect your rewards!")
-		$bot.send_message(125859373393117184, "Weekly gift/ticket reset just happened!\nYour extra ticket storage has been refilled!\nAny weekly events such as Astrolab have been reset!\nDon't forget to collect your rewards!")
-		if File.file?("bot/dailypm")
-			dailypm = loadArr(dailypm,"bot/dailypm")
-		else
-			dailypm = []
-		end
-		dailypm.each { |x| $bot.user(x).pm("Weekly gift/ticket reset just happened!\nYour extra ticket storage has been refilled!\nAny weekly events such as Astrolab have been reset!\nDon't forget to collect your rewards!")}
-	end
-	scheduler.cron '0 11 * * 6' do
-		$bot.send_message(122526505606709257, "Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")
-		$bot.send_message(125859373393117184, "Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")
-		if File.file?("bot/weekendpm")
-			weekendpm = loadArr(weekendpm,"bot/weekendpm")
-		else
-			weekendpm = []
-		end
-		weekendpm.each { |x| $bot.user(x).pm("Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")}
-	end
-	scheduler.cron '0 11 * * 7' do
-		$bot.send_message(122526505606709257, "Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")
-		$bot.send_message(125859373393117184, "Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")
-		if File.file?("bot/weekendpm")
-			weekendpm = loadArr(weekendpm,"bot/weekendpm")
-		else
-			weekendpm = []
-		end
-		weekendpm.each { |x| $bot.user(x).pm("Don't forget to set your computer timezone to Beijing, China and log in to get your tickets. Event starts in 1 hour")}
-	end
-	scheduler.cron '5 */3 * * *' do
-		$bot.stop
-	end
-	puts 'Cron jobs scheduled!'
+	schedule_cron
 	
 	puts 'Sync Confirmed!'
 	puts 'SKYNET ONLINE'
