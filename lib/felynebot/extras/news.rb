6be80926ddc4f2f4c52fa9b547544dd45a680936
Reaver01 <reaver_01@gmx.com>
2016-11-23 21:55:11 +0800

    pass invalid url for testing

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,6 +1,6 @@
 def news_pull
 	begin
-		$currentnews = open('http://mho.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
+		$currentnews = open('http://mh0.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
 		#$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
 		#$currentnews.delete! "\s"
 		#File.write('bot/newnews', $currentnews)
