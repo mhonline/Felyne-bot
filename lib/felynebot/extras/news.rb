bb25117d64a2a43ea83ffc2459eba63df149c44e
Reaver01 <reaver_01@gmx.com>
2016-11-23 21:59:21 +0800

    test other error handling

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,11 +1,12 @@
 def news_pull
-	begin
-		$currentnews = open('http://mh0.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
-		#$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
-		#$currentnews.delete! "\s"
-		#File.write('bot/newnews', $currentnews)
-		#puts "News gathered!"
-	rescue OpenURI::HTTPError => ex
-		puts "News url invalid"
+	url = 'http://mh0.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml'
+	if open(url)
+		$currentnews = open(url).read
+		$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
+		$currentnews.delete! "\s"
+		File.write('bot/newnews', $currentnews)
+		puts "News gathered!"
+	else
+		puts "Can't Connect to news!"
 	end
 end
