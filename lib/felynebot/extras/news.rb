bc778c44c42591d916cc72e16454779460c4a432
Reaver01 <reaver_01@gmx.com>
2016-11-23 21:52:37 +0800

    testing error stuff

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,10 +1,10 @@
 def news_pull
 	begin
 		$currentnews = open('http://mho.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
-		$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
-		$currentnews.delete! "\s"
-		File.write('bot/newnews', $currentnews)
-		puts "News gathered!"
+		#$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
+		#$currentnews.delete! "\s"
+		#File.write('bot/newnews', $currentnews)
+		#puts "News gathered!"
 	rescue OpenURI::HTTPError => ex
 		puts "News url invalid"
 	end
