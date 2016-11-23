60a4230f7b8802bf8b50e4a47bb42f0d71d57732
Reaver01 <reaver_01@gmx.com>
2016-11-23 21:49:56 +0800

    error handling for news 404

--- lib/felynebot/extras/news.rb
+++ lib/felynebot/extras/news.rb
@@ -1,7 +1,11 @@
 def news_pull
-	$currentnews = open('http://mho.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
-	$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
-	$currentnews.delete! "\s"
-	File.write('bot/newnews', $currentnews)
-	puts "news gathered!"
+	begin
+		$currentnews = open('http://mho.qq.com/webplat/info/news_version3/5499/5500/5501/m4027/list_1.shtml').read
+		$currentnews.gsub!(/.*?(?=<ul class="newsList">)/im, "")
+		$currentnews.delete! "\s"
+		File.write('bot/newnews', $currentnews)
+		puts "News gathered!"
+	rescue OpenURI::HTTPError => ex
+		puts "News url invalid"
+	end
 end
