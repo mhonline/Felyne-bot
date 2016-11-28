b82e672511a47650bc76078233f7cf3f1eacc920
Reaver01 <reaver_01@gmx.com>
2016-11-28 17:05:05 +0100

    I suck at this so bad

--- lib/felynebot/commands/admin/ping.rb
+++ lib/felynebot/commands/admin/ping.rb
@@ -9,6 +9,7 @@ module FelyneBot
 			) do |event, ip|
 				if ip == nil
 					ip = "ied-tqos.qq.com"
+				end
 				x = `ping -c 1 #{ip} | grep icmp_seq`
 				event.respond  "#{x}"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: ping"
