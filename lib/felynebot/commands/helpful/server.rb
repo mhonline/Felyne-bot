bf98291926a441d95956521e3c1650fc431cdbbe
Reaver01 <reaver_01@gmx.com>
2016-12-21 07:32:47 +0100

    server switch

--- lib/felynebot/commands/helpful/server.rb
+++ lib/felynebot/commands/helpful/server.rb
@@ -8,7 +8,7 @@ module FelyneBot
 					description: "Displays a picture of the server most of us play on.",
 					useage: "server"
 			) do |event|
-				event.respond "https://i.gyazo.com/120e19545d6b56df935fd24617d47001.jpg"
+				event.respond "http://i.imgur.com/GDMsGNt.png"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: server"
 				nil
 			end
