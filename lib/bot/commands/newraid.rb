674f3148aee54f07038dd91562f623a28c49ff31
Reaver01 <reaver_01@gmx.com>
2017-01-03 23:06:05 +0100

    set up permission level again

--- lib/bot/commands/newraid.rb
+++ lib/bot/commands/newraid.rb
@@ -7,7 +7,10 @@ module MainBot
 					description: "Sets a raid reminder for the channel.",
 					usage: "remindme <days> <hours> <mins> <text>",
 					help_available: true,
-					min_args: 4
+					permission_level: 1,
+					min_args: 4,
+					permission_message: "Ask a Mod or Admin to set up a raid!"
+					
 			) do |event, days, hours, minutes, *text|
 				channelname = event.channel.name
 				if channelname.include? "raid"
