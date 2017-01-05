fdc355d4f7b7fff5ad09c6bd5ebec014c1e768db
Reaver01 <reaver_01@gmx.com>
2017-01-05 19:49:45 +0100

    rescue the whole fucking thing

--- lib/bot/extras/cron.rb
+++ lib/bot/extras/cron.rb
@@ -10,34 +10,33 @@ def cronjobs_start
 		puts "[#{clock.inspect}] Checking for raids"
 
 		Dir["botfiles/raids/*"].each { |file| 
-
-			channel = file
-			channel.slice! "botfiles/raids/"
-			puts ">Loading raids for: #{channel}"
-			raids = loadArr(raids,"botfiles/raids/#{channel}")
-			pos = 0
 			begin
-				t4 = raids[pos]
-				t4 = Time.parse(t4)
-				if t4.past?
-					begin
+				channel = file
+				channel.slice! "botfiles/raids/"
+				puts ">Loading raids for: #{channel}"
+				raids = loadArr(raids,"botfiles/raids/#{channel}")
+				pos = 0
+				begin
+					t4 = raids[pos]
+					t4 = Time.parse(t4)
+					if t4.past?
 						$bot.send_message(channel, "Raid for #{raids[pos+1]}")
-					rescue
-
+						puts ">    Posting raid to #{channel}! Deleting reminder!"
+						raids.delete_at(pos+1)
+						raids.delete_at(pos)
+						pos -= 2
 					end
-					puts ">    Posting raid to #{channel}! Deleting reminder!"
-					raids.delete_at(pos+1)
-					raids.delete_at(pos)
-					pos -= 2
-				end
-				pos += 2
-			end while pos < raids.length
+					pos += 2
+				end while pos < raids.length
 
-			if raids.length == 0
-				File.delete("botfiles/raids/#{channel}")
-				puts ">        No more raids exist for #{channel}! Deleting file!"
-			else
-				File.write("botfiles/raids/#{channel}", raids)
+				if raids.length == 0
+					File.delete("botfiles/raids/#{channel}")
+					puts ">        No more raids exist for #{channel}! Deleting file!"
+				else
+					File.write("botfiles/raids/#{channel}", raids)
+				end
+			rescue
+				puts "There is an error with the raids for #{channel}"
 			end
 		}
 	end
