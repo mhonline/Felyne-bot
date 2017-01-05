bbdfa100e19516a53219ae40da57f49537a908aa
Reaver01 <reaver_01@gmx.com>
2017-01-05 20:56:37 +0100

    don't module it

--- lib/bot/class/embeds.rb
+++ lib/bot/class/embeds.rb
@@ -1,21 +1,19 @@
-class Embeds
-	def list_raids(raids, channel)
-        o = ""
-        x = 0
-        y = 1
-        begin
-                o += "**Reminder #{y}:** #{raids[x+1]} @ #{raids[x]}\n"
-                x += 2
-                y += 1
-        end while x < raids.length
-		e = Discordrb::Webhooks::Embed.new
-		e.author = {
-			name: "Raids for #{channel}"
-		}
-		e.color = 0x673ab7
-		e.description = o
-		e.footer = { text: channel }
-		e.timestamp = Time.now
-		e
-	end
+def list_raids(raids, channel)
+    o = ""
+    x = 0
+    y = 1
+    begin
+            o += "**Reminder #{y}:** #{raids[x+1]} @ #{raids[x]}\n"
+            x += 2
+            y += 1
+    end while x < raids.length
+	e = Discordrb::Webhooks::Embed.new
+	e.author = {
+		name: "Raids for #{channel}"
+	}
+	e.color = 0x673ab7
+	e.description = o
+	e.footer = { text: channel }
+	e.timestamp = Time.now
+	e
 end
