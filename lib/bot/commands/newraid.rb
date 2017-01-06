2f72c70bef66a8fb23c87151701137e7919598f8
Reaver01 <reaver_01@gmx.com>
2017-01-06 22:56:25 +0100

    fix raids push

--- lib/bot/commands/newraid.rb
+++ lib/bot/commands/newraid.rb
@@ -43,7 +43,7 @@ module MainBot
 						t60 = t3 + d*24*60*60
 
 						if $raids.key?(event.channel.id.to_s)
-							$raids[event.channel.id.to_s]['raids'].push([{"name"=>raidtext.to_s, "time"=>tactual.to_s }])
+							$raids[event.channel.id.to_s]['raids'].push({"name"=>raidtext.to_s, "time"=>tactual.to_s })
 						else
 							$raids[event.channel.id.to_s] = {"raids"=>[{"name"=>raidtext.to_s, "time"=>tactual.to_s}]}
 						end
