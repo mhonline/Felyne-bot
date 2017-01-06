41b96b3e034650d0b59e9cb9028420c9c7ce3fe3
Reaver01 <reaver_01@gmx.com>
2017-01-06 23:17:49 +0100

    return 0 if less than 0

--- lib/bot/class/embeds.rb
+++ lib/bot/class/embeds.rb
@@ -10,10 +10,19 @@ def list_raids(raids, channel)
     	h1 = TimeDifference.between(t1, t4).in_hours
     	m1 = TimeDifference.between(t1, t4).in_minutes
     	d2 = d1.floor
+        if d2 < 0
+            d2 = 0            
+        end
     	h1 = h1 - 24 * d2
     	h2 = h1.floor
+        if h2 < 0
+            h2 = 0            
+        end
     	m1 = m1 - 60 * h2 - 24 * 60 * d2
     	m2 = m1.floor
+        if m2 < 0
+            m2 = 0            
+        end
     	if d2 == 0
 			o += "Raid #{y}: #{raids[x]['name'].chomp(' is starting *now*!')} in #{h2} hours #{m2} minutes\n"
 		else
