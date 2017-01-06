47ace04524f106e93948154b9497f01e5aa036f7
Reaver01 <reaver_01@gmx.com>
2017-01-06 22:21:58 +0100

    try this

--- lib/bot/class/raid.rb
+++ lib/bot/class/raid.rb
@@ -1,10 +1,10 @@
 class Raid
-	def initialize(channelid, array)
-		if array[channelid.to_s].any?
-			$bot.send_message(channelid, "channel already has an array initialized")
-		else
-			array[channelid.to_s] = {"raids"=>[ ]}
-			$bot.send_message(channelid, "Channel array initialized")
-		end
+	def initialize(id, ar, n, t)
+		@channel = id
+		@array = ar
+		@name = n
+		@time = t
+		
+		array.push(self)
 	end
 end
