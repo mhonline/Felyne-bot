9a4b3b9fbc077e05ceb371b1b199e37803a031f7
Reaver01 <reaver_01@gmx.com>
2017-01-06 22:05:39 +0100

    change if statemen

--- lib/bot/class/raid.rb
+++ lib/bot/class/raid.rb
@@ -1,6 +1,6 @@
 class Raid
 	def initialize(channelid, array)
-		if array[channelid.to_s]?
+		if array[channelid.to_s].any?
 			$bot.send_message(channelid, "channel already has an array initialized")
 		else
 			array[channelid.to_s] = {'raids'=>[]}
