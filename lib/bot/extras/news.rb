b16a966dff66947640365832b981d4ab75b6bfc2
Reaver01 <reaver_01@gmx.com>
2017-01-07 00:41:27 +0100

    send news to all subbed channels

--- lib/bot/extras/news.rb
+++ lib/bot/extras/news.rb
@@ -42,31 +42,41 @@ def news_post
 
 	if !$news.include? $news6date + " | " + $news6link
 		$news.push($news6date + " | " + $news6link)
-		$bot.send_message(261019677390536704, "#{$news6date} | <http://mho.qq.com#{$news6link}>")
+		$qqnews.each do |key|
+			$bot.send_message(key, "#{$news6date} | <http://mho.qq.com#{$news6link}>")
+		end
 		$news1 = $news.join(",").gsub("\n","")
 		File.write("botfiles/oldnews", $news1)
 	end
 	if !$news.include? $news5date + " | " + $news5link
 		$news.push($news5date + " | " + $news5link)
-		$bot.send_message(261019677390536704, "#{$news5date} | <http://mho.qq.com#{$news5link}>")
+		$qqnews.each do |key|
+			$bot.send_message(key, "#{$news5date} | <http://mho.qq.com#{$news5link}>")
+		end
 		$news1 = $news.join(",").gsub("\n","")
 		File.write("botfiles/oldnews", $news1)
 	end
 	if !$news.include? $news4date + " | " + $news4link
 		$news.push($news4date + " | " + $news4link)
-		$bot.send_message(261019677390536704, "#{$news4date} | <http://mho.qq.com#{$news4link}>")
+		$qqnews.each do |key|
+			$bot.send_message(key, "#{$news4date} | <http://mho.qq.com#{$news4link}>")
+		end
 		$news1 = $news.join(",").gsub("\n","")
 		File.write("botfiles/oldnews", $news1)
 	end
 	if !$news.include? $news3date + " | " + $news3link
 		$news.push($news3date + " | " + $news3link)
-		$bot.send_message(261019677390536704, "#{$news3date} | <http://mho.qq.com#{$news3link}>")
+		$qqnews.each do |key|
+			$bot.send_message(key, "#{$news3date} | <http://mho.qq.com#{$news3link}>")
+		end
 		$news1 = $news.join(",").gsub("\n","")
 		File.write("botfiles/oldnews", $news1)
 	end
 	if !$news.include? $news2date + " | " + $news2link
 		$news.push($news2date + " | " + $news2link)
-		$bot.send_message(261019677390536704, "#{$news2date} | <http://mho.qq.com#{$news2link}>")
+		$qqnews.each do |key|
+			$bot.send_message(key, "#{$news2date} | <http://mho.qq.com#{$news2link}>")
+		end
 		$news1 = $news.join(",").gsub("\n","")
 		File.write("botfiles/oldnews", $news1)
 	end
