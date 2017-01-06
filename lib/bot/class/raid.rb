bb192fa9baba83c4cb5835f2c5fee9cfd49f48ef
Reaver01 <reaver_01@gmx.com>
2017-01-06 22:07:11 +0100

    test

--- lib/bot/class/raid.rb
+++ lib/bot/class/raid.rb
@@ -3,7 +3,7 @@ class Raid
 		if array[channelid.to_s].any?
 			$bot.send_message(channelid, "channel already has an array initialized")
 		else
-			array[channelid.to_s] = {'raids'=>[]}
+			array[channelid.to_s] = {"raids"=>[ ]}
 			$bot.send_message(channelid, "Channel array initialized")
 		end
 	end
