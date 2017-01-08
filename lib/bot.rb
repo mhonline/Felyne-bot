d8d3e1468e6dba861956239546d194b2366ddb12
Reaver01 <reaver_01@gmx.com>
2017-01-08 01:19:28 +0100

    add back silly things

--- lib/bot.rb
+++ lib/bot.rb
@@ -43,6 +43,52 @@ module MainBot
 	Commands.constants.each do |x|
 		$bot.include! Commands.const_get x
 	end
+
+	#Fun Commands
+	$bot.message(containing: "(╯°□°）╯︵ ┻━┻") { |event|
+		event.respond "┬─┬﻿ ノ( ゜-゜ノ)"
+	}
+	$bot.message(containing: "Open the pod bay doors") { |event|
+		role = event.bot.server(122526505606709257).roles.find { |role| role.name == "Felyne" }
+		newcolor = '0x'
+		newcolor << getline("bot/colors",1)
+		newcolor = newcolor.gsub("\n","").hex
+		role.color = Discordrb::ColorRGB.new(newcolor)
+		$bot.profile.avatar = File.open("pic/HAL9000.png")
+		event.respond "Im sorry Dave, Im afraid I can't do that."
+	}
+	$bot.message(containing: "This fucking guy") { |event|
+		role = event.bot.server(122526505606709257).roles.find { |role| role.name == "Felyne" }
+		newcolor = '0x'
+		newcolor << getline("bot/colors",1)
+		newcolor = newcolor.gsub("\n","").hex
+		role.color = Discordrb::ColorRGB.new(newcolor)
+		$bot.profile.avatar = File.open("pic/mhol_christmas2.jpg")
+		event.respond "This fucking guy indeed."
+	}
+	$bot.message(containing: "┬─┬﻿ ノ( ゜-゜ノ)") { |event|
+		event.respond "(╯°□°）╯︵ ┻━┻"
+	}
+	$bot.message(containing: "111") { |event|
+		id_list = ["1112", "1113", "1114", "1115", "1116", "1117", "1118", "1119", "1110", "2111", "3111", "4111", "5111", "6111", "7111", "8111", "9111", "0111"]
+		if id_list.any?{|s| event.message.content.include?(s)}
+		else
+			event.respond "CATCH THE FUCKING MONSTER!!"
+		end
+	}
+	$bot.message(containing: "RIP") { |event|
+		rip = ["Rest in Pizza", "RIP in Peace", "Ripperoni", "Rippy Dippy Doo", "Rest in Peace", "RIP in Pizza"]
+		event.respond rip[rand(0..(rip.length-1))]
+	}
+	$bot.message(containing: ["english patch", "English Patch", "English patch", "english Patch", "Eng Patch", "Eng patch", "eng patch", "eng Patch", "ENGLISH PATCH", "ENG PATCH"]) { |event|
+		engpatch = "2016-06-21 00:00:00 +0000"
+		t1 = Time.now
+		t4 = Time.parse(engpatch)
+		d1 = TimeDifference.between(t1, t4).in_days
+		d2 = d1.floor
+		event.respond "Day #{d2}: Still no english patch"
+	}
+	puts "Commands Loaded"
 	
 	#Turn off debugging and run async
 	$bot.debug = false
