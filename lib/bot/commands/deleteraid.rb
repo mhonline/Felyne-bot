18d000e7147d6c8f666687a50c80c21b87bac1c2
Reaver01 <jawmac@gmail.com>
2017-01-25 11:44:59 +0100

    Update deleteraid.rb

--- lib/bot/commands/deleteraid.rb
+++ lib/bot/commands/deleteraid.rb
@@ -11,8 +11,10 @@ module Commands
     ) do |event, delete|
       delete = delete.to_i
       if $raids.key?(event.channel.id.to_s)
-        $raids[event.channel.id.to_s]['raids'].delete_at(delete - 1) unless delete.zero? || delete > $raids[event.channel.id.to_s]['raids'].length - 1
-        event.respond 'Raid was deleted.'
+        unless delete.zero? || delete > $raids[event.channel.id.to_s]['raids'].length - 1
+          $raids[event.channel.id.to_s]['raids'].delete_at(delete - 1)
+          event.respond 'Raid was deleted.'
+        end
       else
         event << "You do not have any raids set right now. Set some with `#{$prefix}newraid`"
       end
