3c838239ebdeb918d709d2ae408c285cfb6bc38a
Reaver01 <reaver_01@gmx.com>
2017-01-25 19:20:42 +0100

    make sure it can be added to new array

--- lib/bot/commands/newguild.rb
+++ lib/bot/commands/newguild.rb
@@ -8,28 +8,38 @@ module Commands
       usage: 'newguild <GuildName>',
       min_args: 1
     ) do |event, *guild_name|
-      guild_name = guild_name.join(' ')
+      guild_name = guild_name.join(' ').titleize
       if event.user.can_manage_roles?
         server_role = event.server.roles.find { |role| role.name == guild_name }
         if server_role.nil?
           new_role = event.server.create_role
-          new_role.name = guild_name.titleize
+          new_role.name = guild_name
           new_role.color = Discordrb::ColorRGB.new(rand(0xffffff))
           new_role.hoist = true
           new_role.mentionable = true
-          $guilds[event.server.id.to_s].push(
-            'name' => guild_name.titleize, 'id' => new_role.id
-          )
+          if $guilds[event.server.id.to_s].empty?
+            $guilds[event.server.id.to_s] = [{
+              'name' => guild_name, 'id' => new_role.id
+            }]
+          else
+            $guilds[event.server.id.to_s].push(
+              'name' => guild_name, 'id' => new_role.id
+            )
+          end
         elsif $guilds.key?(event.server.id.to_s)
           i = 0
           search = ''
-          until i == $guilds.length || search == guild_name.titleize
+          until i == $guilds.length || search == guild_name
             search = $guilds[event.server.id.to_s][i]
             i += 1
           end
           if search != ''
             event.respond "The #{search} role is already set up on this " \
                           'server'
+          elsif $guilds[event.server.id.to_s].empty?
+            $guilds[event.server.id.to_s] = [{
+              'name' => search, 'id' => server_role.id
+            }]
           else
             $guilds[event.server.id.to_s].push(
               'name' => search, 'id' => server_role.id
