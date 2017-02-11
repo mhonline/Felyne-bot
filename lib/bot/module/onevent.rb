0506a64ece23ec634912a869b60ec7e6e881d9ec
reaver01 <reaver_01@gmx.com>
2017-02-11 14:48:46 +0100

    supress spam

--- lib/bot/module/onevent.rb
+++ lib/bot/module/onevent.rb
@@ -9,39 +9,35 @@ module Events
     event.respond '(╯°□°）╯︵ ┻━┻'
   end
 
-  message(containing: '111') do |event|
-    id_list = %w(1112 1113 1114 1115 1116 1117 1118 1119 1110 2111 3111 4111
-                 5111 6111 7111 8111 9111 0111)
-    if id_list.any? { |s| event.message.content.include?(s) }
-    else
-      event.respond 'CATCH THE FUCKING MONSTER!!'
-    end
-  end
+  # message(containing: '111') do |event|
+  #   id_list = %w(1112 1113 1114 1115 1116 1117 1118 1119 1110 2111 3111 4111
+  #                5111 6111 7111 8111 9111 0111)
+  #   if id_list.any? { |s| event.message.content.include?(s) }
+  #   else
+  #     event.respond 'CATCH THE FUCKING MONSTER!!'
+  #   end
+  # end
 
-  message(containing: 'RIP') do |event|
-    rip = [
-      'Rest in Pizza', 'RIP in Peace', 'Ripperoni', 'Rippy Dippy Doo',
-      'Rest in Peace', 'RIP in Pizza', 'RIP in Pizza Noodle'
-    ]
-    event.respond rip[rand(0..(rip.length - 1))]
-  end
+  # message(containing: 'RIP') do |event|
+  #   rip = [
+  #     'Rest in Pizza', 'RIP in Peace', 'Ripperoni', 'Rippy Dippy Doo',
+  #     'Rest in Peace', 'RIP in Pizza', 'RIP in Pizza Noodle'
+  #   ]
+  #   event.respond rip[rand(0..(rip.length - 1))]
+  # end
 
-  message(containing: [
-            'english patch', 'English Patch', 'English patch', 'english Patch',
-            'Eng Patch', 'Eng patch', 'eng patch', 'eng Patch', 'ENGLISH PATCH',
-            'ENG PATCH'
-          ]) do |event|
-    engpatch = '2016-06-21 00:00:00 +0000'
-    t1 = Time.now
-    t4 = Time.parse(engpatch)
-    d1 = TimeDifference.between(t1, t4).in_days
-    d2 = d1.floor
-    event.respond "Day #{d2}: Still no english patch"
-  end
-
-  message(containing: %w(Aslind aslind Asslind asslind)) do |event|
-    event.respond 'http://imgur.com/wJQ2xpm.png'
-  end
+  # message(containing: [
+  #           'english patch', 'English Patch', 'English patch', 'english Patch',
+  #           'Eng Patch', 'Eng patch', 'eng patch', 'eng Patch', 'ENGLISH PATCH',
+  #           'ENG PATCH'
+  #         ]) do |event|
+  #   engpatch = '2016-06-21 00:00:00 +0000'
+  #   t1 = Time.now
+  #   t4 = Time.parse(engpatch)
+  #   d1 = TimeDifference.between(t1, t4).in_days
+  #   d2 = d1.floor
+  #   event.respond "Day #{d2}: Still no english patch"
+  # end
 
   message do |event|
     unless event.message.content.include?(PREFIX)
@@ -55,9 +51,11 @@ module Events
             event.message.content
           ).id == 192_753_495_806_312_451
             text = event.message.content.delete('<@192753495806312451>')
-            event.channel.start_typing
-            sleep rand(1..3)
-            event.respond CLEVER.say(text)
+            unless event.channel.default?
+              event.channel.start_typing
+              sleep rand(1..3)
+              event.respond CLEVER.say(text)
+            end
           end
         end
         if $users.key?(event.server.id.to_s)
