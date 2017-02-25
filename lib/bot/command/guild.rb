373bd0cd1a60b1800607d41d2a876ea9df18175b
reaver01 <reaver_01@gmx.com>
2017-02-25 18:37:39 +0100

    fix some things

--- lib/bot/command/guild.rb
+++ lib/bot/command/guild.rb
@@ -8,30 +8,30 @@ module Commands
       usage: 'guild <guildname>',
       min_args: 1
     ) do |event, *guild_name|
-      guild_name = guild_name.join(' ').titleize
+      guild_name = guild_name.join(' ').downcase
       found = ''
       added = false
       joinable = true
       if $guilds.key?(event.server.id.to_s)
         i = 0
-        until i == $guilds[event.server.id.to_s].length || found == guild_name
-          found = $guilds[event.server.id.to_s][i]['name'].titleize
+        until i == $guilds[event.server.id.to_s].length || found.downcase == guild_name.downcase
+          found = $guilds[event.server.id.to_s][i]['name'].downcase
           i += 1
         end
       else
         joinable = false
       end
-      if found == guild_name
+      if found.downcase == guild_name.downcase
         $guilds[event.server.id.to_s].each do |x|
           server_role = event.user.roles.find do |role|
-            role.name == x['name']
+            role.name.downcase == x['name'].downcase
           end
           event.user.remove_role(server_role) unless server_role.nil?
         end
         $guilds[event.server.id.to_s].each do |x|
-          next unless guild_name == x['name']
+          next unless guild_name.downcase == x['name'].downcase
           server_role = event.server.roles.find do |role|
-            role.name == x['name']
+            role.name.downcase == x['name'].downcase
           end
           event.user.add_role(server_role)
         end
