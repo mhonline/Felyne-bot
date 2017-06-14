b6e91257f405794429076d3e1bd7d5a104c48b3c
Reaver01 <jawmac@gmail.com>
2017-06-14 18:17:16 -0500

    don't rescue errors

--- lib/bot/module/playing.rb
+++ lib/bot/module/playing.rb
@@ -2,16 +2,13 @@
 module Events
   extend Discordrb::EventContainer
   playing do |event|
-    begin
-      server_role = event.server.roles.find do |role|
-        role.name == "Playing MHO"
-      end
-      if event.game == "Monster Hunter Online"
-        event.user.add_role(server_role)
-      else
-        event.user.remove_role(server_role)
-      end
-    rescue
+    server_role = event.server.roles.find do |role|
+      role.name == "Playing MHO"
+    end
+    if event.game == "Monster Hunter Online"
+      event.user.add_role(server_role)
+    else
+      event.user.remove_role(server_role)
     end
   end
 end
