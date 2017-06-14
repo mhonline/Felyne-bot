30f9a10b1b59a6ee0172764d3e10f1a76674d632
Reaver01 <jawmac@gmail.com>
2017-06-14 18:16:01 -0500

    remove extra end

--- lib/bot/module/playing.rb
+++ lib/bot/module/playing.rb
@@ -1,18 +1,17 @@
 # Adds a role based on who is playing MHO
 module Events
   extend Discordrb::EventContainer
-    playing do |event|
-      begin
-        server_role = event.server.roles.find do |role|
-          role.name == "Playing MHO"
-        end
-        if event.game == "Monster Hunter Online"
-          event.user.add_role(server_role)
-        else
-          event.user.remove_role(server_role)
-        end
-      rescue
+  playing do |event|
+    begin
+      server_role = event.server.roles.find do |role|
+        role.name == "Playing MHO"
       end
+      if event.game == "Monster Hunter Online"
+        event.user.add_role(server_role)
+      else
+        event.user.remove_role(server_role)
+      end
+    rescue
     end
   end
 end
