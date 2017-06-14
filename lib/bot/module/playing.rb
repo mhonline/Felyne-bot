383b64463128a1948c6d30cb7caff48ae2e20eda
Reaver01 <jawmac@gmail.com>
2017-06-14 18:14:43 -0500

    impliment

--- lib/bot/module/playing.rb
+++ lib/bot/module/playing.rb
@@ -1,7 +1,18 @@
 # Adds a role based on who is playing MHO
 module Events
   extend Discordrb::EventContainer
-  playing do |event|
-  	puts event.game
+    playing do |event|
+      begin
+        server_role = event.server.roles.find do |role|
+          role.name == "Playing MHO"
+        end
+        if event.game == "Monster Hunter Online"
+          event.user.add_role(server_role)
+        else
+          event.user.remove_role(server_role)
+        end
+      rescue
+      end
+    end
   end
 end
