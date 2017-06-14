4aa94237af87773fb3c5efb37dea875366c991f9
Reaver01 <jawmac@gmail.com>
2017-06-14 18:43:55 -0500

    testing

--- lib/bot/module/playing.rb
+++ lib/bot/module/playing.rb
@@ -3,8 +3,9 @@ module Events
   extend Discordrb::EventContainer
   playing do |event|
     server_role = event.server.roles.find { |role| role.name == "Playing MHO" }
+    puts server_role.id
     if event.game == "Monster Hunter Online"
-      puts event.user.role?(server_role)
+      # puts event.user.role?(server_role)
       # .add_role(event.server.roles.find { |role| role.name == "Playing MHO" })
     end
   end
